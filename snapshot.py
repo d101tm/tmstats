a930fec1807e008b0a71d189972e18da83a3b20c
David Singer <david@dsinger.com>
2015-03-15 15:30:42 -0700

    Program to create the DEC snapshot.  Initial version.

---
+++ snapshot.py
@@ -0,0 +1,167 @@
+#!/usr/bin/python
+""" Creates a "DEC Snapshot" for the alignment process
+    Uses "alignment.xlsx" and the latest available files in the data directory
+    """
+
+import xlsxwriter, csv, sys, os, glob, codecs, cStringIO, re
+from club import Club
+
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
+def normalize(s):
+    if s:
+        return re.sub(r'[^a-z0-9]','',s.lower())
+    else:
+        return
+
+os.chdir('data')    # To the data directory!
+
+# We need the latest version of all the statistics
+latest = sorted(glob.glob('clubs.*.csv'))[-1].split('.')[1]
+
+# Start with the club listing (which should, one hopes, include everything)
+# But it doesn't.  
+
+clubs = {}
+
+csvfile = open('clubs.' + latest + '.csv', 'rbU')
+r = csv.reader(csvfile, delimiter=',')
+baseheaders = [normalize(p) for p in r.next()]
+Club.setHeaders(baseheaders)
+clubcol = baseheaders.index('clubnumber')
+allheaders = baseheaders
+
+for row in r:
+    try:
+        row = [unicode(x, 'UTF8') for x in row]
+    except UnicodeDecodeError:
+        row = [unicode(x, 'CP1252') for x in row]
+
+    try:
+        row[clubcol] = Club.fixcn(row[clubcol])
+        clubnum = row[clubcol]
+        if clubnum:
+            club = Club(row)
+            clubs[clubnum] = club
+    except IndexError:
+        pass
+
+csvfile.close()
+# OK, now we have the basics.  Now, add relevant performance information
+
+csvfile = open('clubperf.' + latest + '.csv', 'rbU')
+r = csv.reader(csvfile, delimiter=',')
+headers = [normalize(p) for p in r.next()]
+headers[headers.index('clubstatus')] = 'eligibility'  # Avoid name collision
+ourheaders = ['eligibility', 'membase', 'activemembers', 'goalsmet']
+allheaders.extend(ourheaders)
+clubcol = headers.index('clubnumber')
+for row in r:
+    try:
+        row = [unicode(x, 'UTF8') for x in row]
+    except UnicodeDecodeError:
+        row = [unicode(x, 'CP1252') for x in row]
+
+    try:
+        row[clubcol] = Club.fixcn(row[clubcol])
+        clubnum = row[clubcol]
+        if clubnum:
+            if clubnum not in clubs:
+                # We need to patch in information about this club
+                newr = []
+                for item in baseheaders:
+                    try:
+                        newr.append(row[headers.index(item)])
+                    except ValueError:
+                        newr.append('')
+                clubs[clubnum] = Club(newr)
+                clubs[clubnum].clubnumber = clubnum  # Inconsistency, thy name is WHQ
+            # Add our info
+            clubs[clubnum].addinfo(row, headers, ourheaders)
+    except IndexError:
+        pass
+
+csvfile.close()
+
+# And now get the suspend date if the club is suspended...
+csvfile = open('areaperf.' + latest + '.csv', 'rbU')
+r = csv.reader(csvfile, delimiter=',')
+headers = [normalize(p) for p in r.next()]
+headers[headers.index('charterdatesuspenddate')] = 'eventdate'
+ourheaders = ['eventdate']
+allheaders.extend(ourheaders)
+clubcol = headers.index('club')
+for row in r:
+    try:
+        row = [unicode(x, 'UTF8') for x in row]
+    except UnicodeDecodeError:
+        row = [unicode(x, 'CP1252') for x in row]
+
+    try:
+        row[clubcol] = Club.fixcn(row[clubcol])
+        clubnum = row[clubcol]
+        if clubnum:
+            try:
+                clubs[clubnum].addinfo(row, headers, ourheaders)
+            except KeyError:
+                print 'areaperf:', clubnum, ' not in clubs'
+                print row
+    except IndexError:
+        pass
+
+csvfile.close()
+
+allheaders.append('color')
+# Now, some cleanup
+for c in clubs.values():
+    try:
+        c.setcolor()
+    except:
+        c.color = 'Red'
+    el = c.clubstatus.lower()
+    if el.startswith('none') or el.startswith('open'):
+        c.clubstatus = 'Open'
+    else:
+        c.clubstatus = 'Restricted'
+
+csvfile = open('alldata.csv', 'wb')
+w = UnicodeWriter(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+w.writerow(allheaders)
+for c in sorted(clubs.values(), key=lambda x:x.area + x.division + x.clubnumber.zfill(8)):
+    row = []
+    for it in allheaders:
+        try:
+            row.append(c.__dict__[it])
+        except KeyError:
+            row.append('')
+    w.writerow(row)
+
+csvfile.close()
+
