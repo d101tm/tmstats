0bc952fc0170ce1091731f45a50b2fe88ac9c779
David Singer <david@dsinger.com>
2015-06-18 20:12:25 -0700

    Convert numbers and dates to string/unicode.
    Always use fieldnames that come out of the database for 'GetClubsOn'.

--- simpleclub.py
+++ simpleclub.py
@@ -1,5 +1,8 @@
 """ Simple class to maintain information about a club.
 """
+
+import datetime
+
 class Club:
     """ Keep information about a club """
     @classmethod
@@ -12,24 +15,33 @@ class Club:
     def getClubsOn(self, date, curs, setfields=False):
         """ Get the clubs which were in existence on a specified date """
         curs.execute("SELECT * FROM clubs WHERE firstdate <= %s AND lastdate >= %s", (date, date))
+        # Get the fieldnames before we get anything else:
+        fieldnames = [f[0] for f in curs.description]
         if setfields:
-            # Get the fieldnames before we get anything else:
-            fieldnames = [f[0] for f in curs.description]
             Club.setfields(fieldnames)
     
         res = {}
     
         # OK, now build the list of clubs at the beginning of the period
         for eachclub in curs.fetchall():
-            club = Club(eachclub)
+            club = Club(eachclub, fieldnames)
             res[club.clubnumber] = club
             
         return res
         
     
-    def __init__(self, values):
+    def __init__(self, values, fieldnames=None):
         self.cmp = []
-        for (name, value) in zip(self.fieldnames, values):
+        if not fieldnames:
+            fieldnames = self.fieldnames
+        for (name, value) in zip(fieldnames, values):
+            # Let's normalize everything to strings/unicode strings
+            if isinstance(value, (int, long, float, bool)):
+                value = '%s' % value
+            if isinstance(value, bool):
+                value = '1' if value else '0'
+            elif isinstance(value, (datetime.datetime, datetime.date)):
+                value = ('%s' % value)[0:10]
             self.__dict__[name] = value
             if name not in self.badnames:
                 self.cmp.append(value)
@@ -49,4 +61,4 @@ class Club:
         return res
         
     def __repr__(self):
-        return '; '.join(['%s = "%s"' % (name, self.__dict__[name]) for name in self.goodnames])
+        return '; '.join(['%s = "%s"' % (name, self.__dict__[name]) for name in self.__dict__])
