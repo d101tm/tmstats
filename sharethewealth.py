542eaf1d7c9291b77561ccb537b65fbe24d1ea06
David Singer <david@dsinger.com>
2015-06-24 17:35:26 -0700

    Make a pretty report instead of a CSV for individual club stats.

--- sharethewealth.py
+++ sharethewealth.py
@@ -34,19 +34,47 @@ curs = conn.cursor()
 parms.enddate = cleandate(parms.enddate)
 parms.startdate = cleandate(parms.startdate)
 
-print 'checking from %s to %s', (parms.startdate, parms.enddate)
+print 'checking from %s to %s' % (parms.startdate, parms.enddate)
 friendlyenddate = daybefore(parms.enddate)[5:].replace('-','/')
 friendlystartdate = daybefore(parms.startdate)[5:].replace('-','/')
 
 # Open both output files
 divfile = open('sharethewealth.csv', 'wb')
 divwriter = UnicodeWriter(divfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-clubfile = open('sharethewealthclubs.csv', 'wb')
-clubwriter = UnicodeWriter(clubfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+clubfile = open('sharethewealthclubs.html', 'w')
+clubfile.write("""<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Style-Type" content="text/css">
+<title>Share The Wealth</title>
+<style type="text/css">
+
+
+        html {font-family: Arial, "Helvetica Neue", Helvetica, Tahoma, sans-serif;
+              font-size: 75%;}
+      
+        table {width: 75%; font-size: 14px; border-width: 1px; border-spacing: 1px; border-collapse: collapse; border-style: solid;}
+        td, th {border-color: black; border-width: 1px;  vertical-align: middle;
+            padding: 2px; padding-right: 5px; padding-left: 5px; border-style: solid;}
+
+        .name {text-align: left; font-weight: bold; width: 40%;}
+        .number {text-align: right; width: 5%;}
+        
+        .bold {font-weight: bold;}
+        .italic {font-style: italic;}
+        .leader {background-color: aqua;}
+        
+        
+        </style>
+</head>
+<body>
+<h1>Share The Wealth Report</h1>
+<p>Clubs receive $5 in District 4 Credit for every new member added May 20 - June 30.  The club or clubs in each division which adds the most new membes in the division will receive an additional $25 Credit; current leaders are highlighted in the table below.</p>
+""")
 divheaders = ['Division', 'New Members']
-clubheaders = ['Division', 'Club', 'Club Name', 'New Members on %s' % friendlystartdate, 'New Members on %s' % friendlyenddate, 'Members Added']
+clubheaders = ['Division', 'Area', 'Club', 'Club Name', 'New Members on %s' % friendlystartdate, 'New Members on %s' % friendlyenddate, 'Members Added']
 divwriter.writerow(divheaders)
-clubwriter.writerow(clubheaders)
+
 
 # Get performance information from the database for the start date
 clubs = {}
@@ -57,11 +85,31 @@ for (clubnumber, clubname, newmembers) in curs.fetchall():
     clubs[clubnumber].start = newmembers
     clubs[clubnumber].end = newmembers  # So any clubs which fall off end with a zero delta
     
+# Write the table header for the club report
+clubfile.write("""
+<table>
+   <thead>
+     <tr>
+""")
+for h in clubheaders:
+    style = ' style="text-align: left;"' if h=='Club Name' else ''
+    clubfile.write("""
+        <th%s>%s</th>
+""" % (style, h))
+clubfile.write("""
+     </tr>
+   </thead>
+   <tbody>
+""")
+    
 # Now, get information for the end data
+clubsinorder = []
+divmax = {}
 curs.execute("SELECT clubnumber, clubname, area, division, newmembers FROM distperf WHERE asof=%s ORDER BY division, area, clubnumber", (parms.enddate,))
 for (clubnumber, clubname, area, division, newmembers) in curs.fetchall():
     if clubnumber not in clubs:
         clubs[clubnumber] = clubinfo(clubnumber, clubname)
+    clubsinorder.append(clubnumber)
     
 
     club = clubs[clubnumber]
@@ -72,9 +120,33 @@ for (clubnumber, clubname, area, division, newmembers) in curs.fetchall():
     if club.delta > 0:
         if division not in divisions:
             divisions[division] = 0
+            divmax[division] = 0
+        if club.delta > divmax[division]:
+            divmax[division] = club.delta
         divisions[division] += club.delta
-        clubwriter.writerow(['%s' % x for x in (club.division, club.clubnumber, club.clubname, 
-                            club.start, club.end, club.delta)])
+        
+divmax['0D'] = 999999 # Avoid special casing...
+        
+for clubnumber in clubsinorder:
+    club = clubs[clubnumber]
+    if club.delta > 0:
+        clubfile.write('      <tr%s>\n' % (' class="leader"' if club.delta == divmax[club.division] else ''))
+        clubfile.write('        <td class="number">%s</td>\n' % club.division)
+        clubfile.write('        <td class="number">%s</td>\n' % club.area)
+        clubfile.write('        <td class="number">%s</td>\n' % club.clubnumber)
+        clubfile.write('        <td class="name%s">%s</td>\n' % (" bold italic" if club.delta == divmax[club.division] else "", club.clubname))
+        clubfile.write('        <td class="number">%s</td>\n' % club.start)
+        clubfile.write('        <td class="number">%s</td>\n' % club.end)
+        clubfile.write('        <td class="number%s">%s</td>\n' % (" bold italic" if club.delta == divmax[club.division] else "", club.delta))
+        clubfile.write('     </tr>\n')
+
+                            
+# Close out the club file
+clubfile.write("""  </tbody>
+</table>
+</body>
+</html>
+""")
 
 divs = sorted([d for d in divisions.keys() if not d.startswith('0')])
 for d in divs:
