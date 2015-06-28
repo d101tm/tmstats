f2b60f009a21dc2675ac2c729905d0dded64f6d1
David Singer <david@dsinger.com>
2015-06-27 20:19:01 -0700

    Shell script to clear and reload the tables.

---
+++ reload.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Reload the database.
+cd data
+foo=$(grep 'dbpass:' tmstats.yml);export dbpass=${foo##* }
+foo=$(grep 'dbuser:' tmstats.yml);export dbuser=${foo##* }
+foo=$(grep 'dbname:' tmstats.yml);export dbname=${foo##* }
+cd ..
+mysql --user=$dbuser --password=$dbpass $dbname <emptytables.sql
+cd data
+unzip -n history.zip   
+../loaddb.py
+
