4e94cb487cd8036f9b89a7565d3f57476ab4401a
David Singer <david@dsinger.com>
2015-07-20 22:02:45 -0700

    Allow a date for currency; otherwise, accept "yesterday" for perf tables.

--- currency.py
+++ currency.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
-""" Check currency of all four tables.  If no date specified on the command line's --date argument, use today.
+""" Check currency of all four tables for the date specified.
+    If no date is specified, we want the 'clubs' table to be current for today;
+      the other tables can be for today OR yesterday because they are one day behind on Toastmasters.
 
     Return 0 if all four tables are current.
     Return 1 if only the clubs table is outdated.
@@ -8,8 +10,8 @@
     Uses the 'loaded' table.
     
 """
-import tmparms, dbconn, argparse, os, sys
-from datetime import datetime
+import tmparms, dbconn, argparse, os, sys, tmutil
+from datetime import datetime, timedelta
 
 
 if 'TM_DIRECTORY' in os.environ:
@@ -19,25 +21,37 @@ if 'TM_DIRECTORY' in os.environ:
 # Add the '--date' argument to the parser
 parms = tmparms.tmparms()
 today = datetime.today()
-parms.parser.add_argument("--date", dest='date', default=datetime.today().strftime('%Y-%m-%d'))
+yesterday = today - timedelta(1)
+today = today.strftime('%Y-%m-%d')
+yesterday = yesterday.strftime('%Y-%m-%d')
+parms.parser.add_argument("--date", dest='date', default=None)
 parms.parse()
 
 
 conn = dbconn.dbconn(parms.dbhost, parms.dbuser, parms.dbpass, parms.dbname)
 curs = conn.cursor()
 date = parms.date
-print "SELECT tablename FROM loaded WHERE loadedfor='%s'"% (date)
-
-curs.execute("SELECT tablename FROM loaded WHERE loadedfor='%s'"% (date))
 have = {}
 want = ['clubs', 'clubperf', 'distperf', 'areaperf']
 for x in want:
     have[x] = False
 count = 0
-for x in curs.fetchall():
-    have[x[0]] = True
-    count += 1
-
+if date:
+    date = tmutil.cleandate(date)
+    # All tables have to be there for the specified date.
+    curs.execute("SELECT tablename FROM loaded WHERE loadedfor='%s'"% (date))
+    for x in curs.fetchall():
+        have[x[0]] = True
+        count += 1
+else:
+    curs.execute("SELECT tablename FROM loaded WHERE (loadedfor=%s OR loadedfor=%s) AND tablename IN ('clubperf', 'distperf', 'areaperf') GROUP BY tablename", (today, yesterday))
+    for x in curs.fetchall():
+        have[x[0]] = True
+        count += 1
+    curs.execute("SELECT tablename FROM loaded WHERE loadedfor=%s AND tablename = 'clubs'", (today,))
+    for x in curs.fetchall():
+        have[x[0]] = True
+        count += 1
 if count == len(want):
     sys.exit(0)   # Success!
 if count == 3 and not have['clubs']:
