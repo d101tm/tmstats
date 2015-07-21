c746e05268f3a01cffb38122b8146f4ad9bf9cfa
David Singer <david@dsinger.com>
2015-07-21 15:47:47 -0700

    Make it possible to use this as a callable routine in a module
    Still works from the command line, of course.

--- latest.py
+++ latest.py
@@ -6,23 +6,27 @@
 import tmparms, dbconn, os, sys, tmutil
 
 
-if 'TM_DIRECTORY' in os.environ:
-    os.chdir(os.path.join(os.environ['TM_DIRECTORY'],'data'))
 
 
-parms = tmparms.tmparms()
-# Allow specifying a table.
-parms.add_argument('--table', dest='table', default='clubperf')
-parms.parse()
+def getlatest(table, conn):
+    curs = conn.cursor()
+    # The MySQLdb library doesn't allow interpolating the table name, so we do
+    # it via normal Python.
+    statement = 'select t.month, l.latest FROM %s t INNER JOIN (select max(loadedfor) as latest FROM loaded WHERE tablename="%s") l ON t.asof = l.latest GROUP BY t.month, l.latest' % (table, table)
+    curs.execute(statement)
+    ans = curs.fetchone()
+    ans = [tmutil.stringify(x) for x in ans]
+    return ans
 
+if __name__ == '__main__':
+    if 'TM_DIRECTORY' in os.environ:
+        os.chdir(os.path.join(os.environ['TM_DIRECTORY'],'data'))
 
-conn = dbconn.dbconn(parms.dbhost, parms.dbuser, parms.dbpass, parms.dbname)
-curs = conn.cursor()
-
-# The MySQLdb library doesn't allow interpolating the table name, so we do
-# it via normal Python.
-statement = 'select t.month, l.latest FROM %s t INNER JOIN (select max(loadedfor) as latest FROM loaded WHERE tablename="%s") l ON t.asof = l.latest GROUP BY t.month, l.latest' % (parms.table, parms.table)
-curs.execute(statement)
-ans = curs.fetchone()
-ans = [tmutil.stringify(x) for x in ans]
-print ' '.join(ans)
+    parms = tmparms.tmparms()
+    # Allow specifying a table.
+    parms.add_argument('--table', dest='table', default='clubperf')
+    parms.parse()
+    conn = dbconn.dbconn(parms.dbhost, parms.dbuser, parms.dbpass, parms.dbname)
+    
+    ans = getlatest(parms.table, conn)
+    print ' '.join(ans)
