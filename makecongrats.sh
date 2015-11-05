e9193b5b405b360bd9e94440124f42ba98d5504c
David Singer <david@dsinger.com>
2015-11-04 17:50:15 -0800

    Add font parameter; default is Helvetica Bold.

--- makecongrats.sh
+++ makecongrats.sh
@@ -12,14 +12,15 @@ let sline1=line1+shadow
 let sline2=line2+shadow
 let sline3=line3+shadow
 let sline4=line4+shadow
+font="Helvetica-Bold"
 
-
-while getopts "d:c:m:" flag
+while getopts "d:c:m:f:" flag
 do
     case "$flag" in
         d) district=$OPTARG;;
         c) count=$OPTARG;;
         m) month=$OPTARG;;
+        f) font=$OPTARG;;
         ?) exit 1;;
     esac
 done
@@ -32,4 +33,4 @@ if [ -z $district -o -z $count -o -z $month ] ; then
     exit 1
 fi
 
-convert ~/Downloads/ed-achieve-base.jpg  -quality 85 -font /Library/Fonts/Microsoft/Arial\ Bold.ttf   -pointsize 26 -fill  black -draw "text $sleft,$sline1 'Congratulations to the $count District $district'" -draw "text $sleft,$sline2 'members who achieved one or more'" -draw "text $sleft,$sline3 'of their educational goals in $month.'" -draw "text $sleft,$sline4 'Will you be recognized next?'" -fill white   -draw "text $left,$line1 'Congratulations to the $count District $district'" -draw "text $left,$line2 'members who achieved one or more'" -draw "text $left,$line3 'of their educational goals in $month.'" -draw "text $left,$line4 'Will you be recognized next?'" out.jpg && open out.jpg  && ls -la out.jpg
+convert ~/Downloads/ed-achieve-base.jpg  -quality 85 -font "$font"   -pointsize 26 -fill  black -draw "text $sleft,$sline1 'Congratulations to the $count District $district'" -draw "text $sleft,$sline2 'members who achieved one or more'" -draw "text $sleft,$sline3 'of their educational goals in $month.'" -draw "text $sleft,$sline4 'Will you be recognized next?'" -fill white   -draw "text $left,$line1 'Congratulations to the $count District $district'" -draw "text $left,$line2 'members who achieved one or more'" -draw "text $left,$line3 'of their educational goals in $month.'" -draw "text $left,$line4 'Will you be recognized next?'" out.jpg && open out.jpg  && ls -la out.jpg
