12d7fa800d4585974b3abeb8180c263037fad0a5
David Singer <david@dsinger.com>
2015-11-05 16:48:47 -0800

    Defend against blanks; add quality parameter.

--- makecongrats.sh
+++ makecongrats.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-cd ~/Downloads
 let spacing=32
 let line1=100
 let left=420
@@ -13,19 +12,21 @@ let sline2=line2+shadow
 let sline3=line3+shadow
 let sline4=line4+shadow
 font="Helvetica-Bold"
+quality=50
 
-while getopts "d:c:m:f:" flag
+while getopts "d:c:m:f:q:" flag
 do
     case "$flag" in
         d) district=$OPTARG;;
         c) count=$OPTARG;;
         m) month=$OPTARG;;
         f) font=$OPTARG;;
+        q) quality=$OPTARG;;
         ?) exit 1;;
     esac
 done
 
-if [ -z $district -o -z $count -o -z $month ] ; then
+if [ -z "$district" -o -z "$count" -o -z "$month" ] ; then
     echo 'District, Count, and Month are all required'
     echo "district $district"
     echo "count $count"
@@ -33,4 +34,6 @@ if [ -z $district -o -z $count -o -z $month ] ; then
     exit 1
 fi
 
-convert ~/Downloads/ed-achieve-base.jpg  -quality 85 -font "$font"   -pointsize 26 -fill  black -draw "text $sleft,$sline1 'Congratulations to the $count District $district'" -draw "text $sleft,$sline2 'members who achieved one or more'" -draw "text $sleft,$sline3 'of their educational goals in $month.'" -draw "text $sleft,$sline4 'Will you be recognized next?'" -fill white   -draw "text $left,$line1 'Congratulations to the $count District $district'" -draw "text $left,$line2 'members who achieved one or more'" -draw "text $left,$line3 'of their educational goals in $month.'" -draw "text $left,$line4 'Will you be recognized next?'" out.jpg && open out.jpg  && ls -la out.jpg
+cd data
+
+convert ed-achieve-base.png  -quality "$quality" -font "$font"   -pointsize 26 -fill  black -draw "text $sleft,$sline1 'Congratulations to the $count District $district'" -draw "text $sleft,$sline2 'members who achieved one or more'" -draw "text $sleft,$sline3 'of their educational goals in $month.'" -draw "text $sleft,$sline4 'Will you be recognized next?'" -fill white   -draw "text $left,$line1 'Congratulations to the $count District $district'" -draw "text $left,$line2 'members who achieved one or more'" -draw "text $left,$line3 'of their educational goals in $month.'" -draw "text $left,$line4 'Will you be recognized next?'" ed-achieve.jpg && open ed-achieve.jpg  && ls -la ed-achieve.jpg
