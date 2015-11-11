451e57b88e6d4ebdaa3b0bb3a1df51c7c7d10441
David Singer <david@dsinger.com>
2015-11-11 11:22:53 -0800

    Collapse state names into their abbreviations.

--- loaddb.py
+++ loaddb.py
@@ -15,6 +15,61 @@ changecount = 0
 
 headersnotinboth = []
 
+
+statelist = {
+'Alabama':'AL',
+'Alaska':'AK',
+'Arizona':'AZ',
+'Arkansas':'AR',
+'California':'CA',
+'Colorado':'CO',
+'Connecticut':'CT',
+'Delaware':'DE',
+'District of Columbia':'DC',
+'Florida':'FL',
+'Georgia':'GA',
+'Hawaii':'HI',
+'Idaho':'ID',
+'Illinois':'IL',
+'Indiana':'IN',
+'Iowa':'IA',
+'Kansas':'KS',
+'Kentucky':'KY',
+'Louisiana':'LA',
+'Maine':'ME',
+'Maryland':'MD',
+'Massachusetts':'MA',
+'Michigan':'MI',
+'Minnesota':'MN',
+'Mississippi':'MS',
+'Missouri':'MO',
+'Montana':'MT',
+'Nebraska':'NE',
+'Nevada':'NV',
+'New Hampshire':'NH',
+'New Jersey':'NJ',
+'New Mexico':'NM',
+'New York':'NY',
+'North Carolina':'NC',
+'North Dakota':'ND',
+'Ohio':'OH',
+'Oklahoma':'OK',
+'Oregon':'OR',
+'Pennsylvania':'PA',
+'Rhode Island':'RI',
+'South Carolina':'SC',
+'South Dakota':'SD',
+'Tennessee':'TN',
+'Texas':'TX',
+'Utah':'UT',
+'Vermont':'VT',
+'Virginia':'VA',
+'Washington':'WA',
+'West Virginia':'WV',
+'Wisconsin':'WI',
+'Wyoming':'WY'
+} 
+
 def inform(*args, **kwargs):
     """ Print information to 'file' unless suppressed by the -quiet option.
           suppress is the minimum number of 'quiet's that need be specified for
@@ -136,6 +191,7 @@ def doDailyClubs(infile, conn, cdate, firsttime=False):
     
     areacol = dbheaders.index('area')
     divisioncol = dbheaders.index('division')
+    statecol = dbheaders.index('state')
     
     # Now, suppress anything in the file that's not in the database:
     suppress = []
@@ -194,6 +250,10 @@ def doDailyClubs(infile, conn, cdate, firsttime=False):
         except ValueError:
             row[areacol] = thedivision
             row[divisioncol] = thearea
+            
+        # Collapse state names into their abbreviations
+        if row[statecol] in statelist:
+            row[statecol] = statelist[row[statecol]]
             
         # Get the right number of items into the row by setting today as the 
         #   tentative first and last date
