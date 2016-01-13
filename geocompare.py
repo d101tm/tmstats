e935eb5eb72f54d61ebb47db4235ca5707de54c1
David Singer <david@dsinger.com>
2016-01-13 08:48:58 -0800

    Embeds maps.  Too many, need to break into divisional files.

--- geocompare.py
+++ geocompare.py
@@ -63,9 +63,10 @@ class Clubinfo:
         fields = [f.strip() for f in 'district, division, area, clubnumber, clubname, place, address, latitude, longitude, locationtype, whqlatitude, whqlongitude, whqreverse, whqreversetype, maplatitude, maplongitude, mapaddress, mapreverse, mapreversetype'.split(',')]
         for (name, value) in zip(fields, row):
             self.__dict__[name] = value
-        self.whqgeodelta = distance_on_unit_sphere(self.whqlatitude, self.whqlongitude, self.latitude, self.longitude)
-        self.whqmapdelta = distance_on_unit_sphere(self.whqlatitude, self.whqlongitude, self.maplatitude, self.maplongitude)
+        self.whqgeodelta = distance_on_unit_sphere(self.whqlatitude, self.whqlongitude, self.latitude, self.longitude) if (self.whqlatitude != 0 and self.whqlongitude !=0) else 0
+        self.whqmapdelta = distance_on_unit_sphere(self.whqlatitude, self.whqlongitude, self.maplatitude, self.maplongitude) if (self.whqlatitude != 0 and self.whqlongitude !=0) else 0
         self.geomapdelta = distance_on_unit_sphere(self.latitude, self.longitude, self.maplatitude, self.maplongitude)
+        self.maxdelta = max(self.whqgeodelta, self.whqmapdelta, self.geomapdelta)
         self.clubs.append(self)
         locator = 'District %s, Area %s' % (self.district, self.division + self.area)
         if locator not in self.clubsbylocator:
@@ -75,21 +76,9 @@ class Clubinfo:
     def __repr__(self):
         ans = []
         ans.append("<tr>")
-        ans.append("<td>%s</td>"% self.district)
-        ans.append("<td>%s%s</td>"% (self.division, self.area))
         ans.append("<td>%s</td>"% self.clubnumber)
         ans.append("<td>%s</td>"% self.clubname)
         ans.append("<td>")
-        ans.append("<table border=\"1\">")
-        ans.append("<tr>")
-        ans.append("<td width=\"50%%\">%s<br />%s<br />&nbsp;<br />%s</td>"% (self.place, self.address, self.locationtype))
-        ans.append("<td width=\"50%%\">%s</td>"% (self.mapaddress))
-        ans.append("</tr>")
-        ans.append("<tr>")
-        ans.append("<td width=\"50%%\">%s<br />&nbsp;<br />%s</td>"% (self.whqreverse, self.whqreversetype))
-        ans.append("<td width=\"50%%\">%s<br />&nbsp;<br />%s</td>"% (self.mapreverse, self.mapreversetype))
-        ans.append("</tr>")
-        ans.append("<tr><td>WHQ to geoencoded: %.2f; WHQ to map: %.2f; map to geoencoded: %.2f"% (self.whqgeodelta, self.whqmapdelta, self.geomapdelta))
         href = []
         href.append("http://maps.googleapis.com/maps/api/staticmap?size=600x600")
         href.append("&markers=color:red%7Clabel:M%7C")
@@ -100,9 +89,15 @@ class Clubinfo:
             href.append("&markers=color:green%7Clabel:W%7C")
             href.append("%f,%f"% (self.whqlatitude, self.whqlongitude))
         href = ''.join(href)
-        ans.append('<a target="_blank"href="%s">map</a>' % href)
-        ans.append('</td></tr>')
-        ans.append("</table>")
+        ans.append("<img src=\"%s\">" % href)
+        ans.append("<p>Meeting location in Club Central: %s<br />%s</p>\n" % (self.place, self.address)) 
+        ans.append("<p>Meeting location in D4 map:%s</p>" % self.mapaddress)
+        ans.append("<p>Reverse-located (%s) from Club Central: %s</p>" % (self.whqreversetype, self.whqreverse))
+        ans.append("<p>Reverse-located (%s) from D4 map:%s</p>" % (self.mapreversetype, self.mapreverse))
+        ans.append("<p>Coordinates in Club Central (%f, %f)</p>" % (self.whqlatitude, self.whqlongitude))
+        ans.append("<p>Coordinates in D4 map (%f, %f)</p>" % (self.maplatitude, self.maplongitude))
+        ans.append("<p>Coordinates relocated (%f, %f)</p>" % (self.latitude, self.longitude))
+        ans.append("<p>WHQ to geoencoded: %.2f; WHQ to map: %.2f; map to geoencoded: %.2f</p>"% (self.whqgeodelta, self.whqmapdelta, self.geomapdelta))
         ans.append("</td>")
         ans.append("</tr>")
         return '\n'.join(ans)
@@ -130,15 +125,20 @@ if __name__ == "__main__":
 
     # And now, create the final comparison as a table
     outfile = open('compare.html', 'w')
-    outfile.write("""<html>
-  <body>
-    <table>
-""")
+    outfile.write("<html><head>\n")
+    outfile.write("</head><body>\n")
     c.execute("select clubs.district, clubs.division, clubs.area, geo.clubnumber, geo.clubname, geo.place, concat(geo.address,', ',geo.city,', ',geo.state,' ',geo.zip, ', USA'), geo.latitude, geo.longitude, locationtype, geo.whqlatitude, geo.whqlongitude, reverse, reversetype, map.lat, map.lng, map.address, mapreverse, mapreversetype from geo inner join map on map.clubnumber = geo.clubnumber inner join clubs on geo.clubnumber = clubs.clubnumber WHERE clubs.lastdate IN (SELECT MAX(lastdate) FROM clubs) order by clubs.district, clubs.division, clubs.area, geo.clubnumber")
     for (row) in c.fetchall():
         club = Clubinfo(row)
-        outfile.write(repr(club))
-
+    
+    for l in sorted(club.clubsbylocator.keys()):
+        clubs = club.clubsbylocator[l]
+        outfile.write('<h3>%s</h3>\n' % l)
+        clubs.sort(key=lambda k:-k.maxdelta)
+        outfile.write('<table border="1">')
+        for club in clubs:
+            outfile.write(repr(club))
+        outfile.write('</table>')
    
     outfile.write("    </table>\n  </body>\n</html>\n")
     outfile.close()
