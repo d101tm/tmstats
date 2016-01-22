4d6019af7d8efb30aebe065504c53fe7e6fc610d
David Singer <david@dsinger.com>
2016-01-21 22:23:33 -0800

    Generate spreadsheet with "suspicious" address mismatches and improve HTML output

--- geocompare.py
+++ geocompare.py
@@ -6,6 +6,7 @@ import dbconn, tmparms
 import pprint
 import dbconn, tmutil, sys, os
 import json
+import csv
 from uncodeit import myclub
 
 style = """
@@ -138,14 +139,61 @@ def distance_on_unit_sphere(lat1, long1, lat2, long2):
 
 ### Insert classes and functions here.  The main program begins in the "if" statement below.
 
+repl = (('Wy', 'Way'),
+        ('St', 'Street'),
+        ('Ave', 'Avenue'),
+        ('Dr', 'Drive'),
+        ('Fl', 'Floor'),
+        ('Pl', 'Place'),
+        ('Rd', 'Road'),
+        ('Bldg', 'Building'),
+        ('Blvd', 'Boulevard'),
+        ('Pkwy', 'Parkway'),
+        ('Ct', 'Court'),
+        ('Cir', 'Circle'),
+        ('Ctr', 'Center'),
+        ('Suite', ''),
+        ('Ste', ''),
+        ('Apt', ''),
+        ('N', 'North'),
+        ('S', 'South'),
+        ('E', 'East'),
+        ('W', 'West'))
+        
+repdict = {}
+for a,b in repl:
+    a = a.lower()
+    b = b.lower()
+    repdict[a] = b
+    repdict[b] = b
+    
+
+def cleanaddr(what):
+    """ Try to clean up address formats """
+    words = what.lower().replace('#','').replace('-','').replace('.','').split()
+    cleaned = []
+    for w in words:
+        if w in repdict:
+            cleaned.append(repdict[w])
+        else:
+            cleaned.append(w)
+    return ' '.join(cleaned)
+        
+
+
 class Clubinfo:
     clubs = []
     clubsbylocator = {}
     def __init__(self, row):
-        fields = [f.strip() for f in 'district, division, area, clubnumber, clubname, place, address, latitude, longitude, reverse, reversetype, locationtype, whqlatitude, whqlongitude, whqreverse, whqreversetype'.split(',')]
+        fields = [f.strip() for f in 'district, division, area, clubnumber, clubname, place, address, latitude, longitude, reverse, reversetype, locationtype, whqlatitude, whqlongitude, whqreverse, whqreversetype, formatted'.split(',')]
         for (name, value) in zip(fields, row):
             self.__dict__[name] = value
         self.whqgeodelta = distance_on_unit_sphere(self.whqlatitude, self.whqlongitude, self.latitude, self.longitude) if (self.whqlatitude != 0 and self.whqlongitude !=0) else 0
+        astart = cleanaddr(self.address.split(',')[0])
+        fstart = cleanaddr(self.formatted.split(',')[0])
+        rstart = cleanaddr(self.reverse.split(',')[0])
+        self.examineinput = astart != fstart
+        self.examineoutput = fstart != rstart
         self.clubs.append(self)
         locator = 'District %s, Area %s' % (self.district, self.division + self.area)
         if locator not in self.clubsbylocator:
@@ -160,7 +208,7 @@ class Clubinfo:
         ans.append("<tbody>")
         ans.append("<tr>")
         ans.append("<td style=\"vertical-align: top\">")
-        ans.append("<table class=\"deltatable\">")
+        ans.append("<table border=\"1\">")
         ans.append("<tbody>")
         ans.append("<tr>")
         ans.append("<td>Meeting Place in Club Central</td>")
@@ -169,21 +217,25 @@ class Clubinfo:
         ans.append("<td>Meeting Address in Club Central</td>")
         ans.append("<td>%s</td>" % (self.address)) 
         ans.append("</tr><tr>")
-        ans.append("<td>Coordinates in Club Central</td>")
-        ans.append("<td>(%f, %f)</td>" % (self.whqlatitude, self.whqlongitude))
-        ans.append("</tr><tr>")
-        ans.append("<td>which maps to</td>")
-        ans.append("<td>%s<br />(%s)" % (self.whqreverse, self.whqreversetype))
-        ans.append("</tr><tr>")
-        ans.append("<td>Coordinates on D4TM.ORG</td>")
-        ans.append("<td>(%f, %f)</td>" % (self.latitude, self.longitude))
-        ans.append("</tr><tr>")
-        ans.append("<td>which maps to</td>")
+        if (self.examineinput):
+            ans.append("<td>Address as interpreted by Google</td>")
+            ans.append("<td>%s</td>" % (self.formatted))
+            ans.append("</tr><tr>")
+        ans.append("<td>Map Pin (D) on d4tm.org</td>")
         ans.append("<td>%s<br />(%s)" % (self.reverse, self.reversetype))
         ans.append("<td>&nbsp;</td>")
         ans.append("</tr><tr>")
-        ans.append("<td>Distance (miles) between coordinates</td>")
+        ans.append("<td>Map Pin (T) on toastmasters.org</td>")
+        ans.append("<td>%s<br />(%s)" % (self.whqreverse, self.whqreversetype))
+        ans.append("</tr><tr>")
+        ans.append("<td>Distance (miles) between map pins</td>")
         ans.append("<td>%s</td>" % ('%.2f' % self.whqgeodelta if self.whqlatitude != 0 and self.whqlongitude != 0 else 'N/A'))
+        ans.append("</tr><tr>")
+        ans.append("<td>Coordinates on Club Central</td>")
+        ans.append("<td>(%f, %f)</td>" % (self.whqlatitude, self.whqlongitude))
+        ans.append("</tr><tr>")
+        ans.append("<td>Coordinates on D4TM.ORG</td>")
+        ans.append("<td>(%f, %f)</td>" % (self.latitude, self.longitude))
         ans.append("</tr>")
         if club.clubnumber in addrchanges:
             ans.append('<tr><td>Address last changed</td><td>%s</td></tr>' % addrchanges[club.clubnumber])
@@ -196,10 +248,10 @@ class Clubinfo:
         ans.append("<td>")
         href = []
         href.append("http://maps.googleapis.com/maps/api/staticmap?size=400x400&key=AIzaSyAMfs7vnmebebI4j00oTz8kqH3hw7b9s_8") 
-        href.append("&markers=color:blue%7Clabel:G%7C")
+        href.append("&markers=color:blue%7Clabel:D%7C")
         href.append("%f,%f"% (self.latitude, self.longitude))
         if self.whqlatitude != 0.0 or self.whqlongitude != 0.0:
-            href.append("&markers=color:green%7Clabel:C%7C")
+            href.append("&markers=color:green%7Clabel:T%7C")
             href.append("%f,%f"% (self.whqlatitude, self.whqlongitude))
         href = ''.join(href)
         ans.append("<img src=\"%s\">" % href)
@@ -231,7 +283,7 @@ if __name__ == "__main__":
 
     # And now, create the final comparison as a table
 
-    c.execute("select clubs.district, clubs.division, clubs.area, geo.clubnumber, geo.clubname, geo.place, concat(geo.address,', ',geo.city,', ',geo.state,' ',geo.zip, ', USA'), geo.latitude, geo.longitude, geo.reverse, geo.reversetype, geo.locationtype, geo.whqlatitude, geo.whqlongitude, geo.whqreverse, geo.whqreversetype from geo inner join map on map.clubnumber = geo.clubnumber inner join clubs on geo.clubnumber = clubs.clubnumber WHERE clubs.lastdate IN (SELECT MAX(lastdate) FROM clubs) order by clubs.district, clubs.division, clubs.area, geo.clubnumber")
+    c.execute("select clubs.district, clubs.division, clubs.area, geo.clubnumber, geo.clubname, geo.place, concat(geo.address,', ',geo.city,', ',geo.state,' ',geo.zip, ', USA'), geo.latitude, geo.longitude, geo.reverse, geo.reversetype, geo.locationtype, geo.whqlatitude, geo.whqlongitude, geo.whqreverse, geo.whqreversetype, geo.formatted from geo inner join map on map.clubnumber = geo.clubnumber inner join clubs on geo.clubnumber = clubs.clubnumber WHERE clubs.lastdate IN (SELECT MAX(lastdate) FROM clubs) order by clubs.district, clubs.division, clubs.area, geo.clubnumber")
     for (row) in c.fetchall():
         club = Clubinfo(row)
         
@@ -244,6 +296,15 @@ if __name__ == "__main__":
             coordchanges[clubnumber] = changedate
         else:
             addrchanges[clubnumber] = changedate
+            
+    # And now, create pretty web pages and a spreadsheet
+    
+    csvfile = open('compare.csv', 'wb')
+    writer = csv.writer(csvfile)
+    sheetfields = ['district', 'division', 'area', 'clubnumber', 'clubname', 'place', 'examineinput',
+                    'examineoutput', 'address', 'formatted', 'whqreverse', 'reverse']
+    writer.writerow(sheetfields)
+    
     
     for l in sorted(club.clubsbylocator.keys()):
         clubs = club.clubsbylocator[l]
@@ -258,6 +319,8 @@ if __name__ == "__main__":
 
         for club in clubs:
             outfile.write(repr(club))
+            writer.writerow([club.__dict__[k] for k in sheetfields])
            
         outfile.write("</body>\n</html>\n")
         outfile.close()
+    csvfile.close()
