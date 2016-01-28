0c3b0408db5d4a7ad73e05531c2e705c74d4e334
David Singer <david@dsinger.com>
2016-01-27 22:31:50 -0800

    Tolerate clubs in the relocation file not existing.

--- overridepositions.py
+++ overridepositions.py
@@ -21,20 +21,21 @@ def overrideClubPositions(clubs, overridefile, apikey):
     reader = csv.DictReader(data)
     for row in reader:
         clubnumber = row['clubnumber']
-        club = clubs[clubnumber]
-        try:
-            # Use explicit coordinates if we find them
-            latitude = float(row['latitude'])
-            longitude = float(row['longitude'])
-            Relocate(clubnumber, latitude, longitude)   # Create the relocation entry
-            # Both are specified, so we use them
-        except:
-            if row['address']:
-                # No coordinates provided but an address is here; use it.
-                gres = gmaps.geocode(row['address']) # Includes city, state, zip, country if needed
-                reloinfo = myclub(clubnumber, club.clubname, club.place, club.address, club.city, club.state, club.zip, club.country, 0.0, 0.0)
-                reloinfo.process(gres)
-                Relocate(clubnumber, reloinfo.latitude, reloinfo.longitude)
+        if clubnumber in clubs:
+            club = clubs[clubnumber]
+            try:
+                # Use explicit coordinates if we find them
+                latitude = float(row['latitude'])
+                longitude = float(row['longitude'])
+                Relocate(clubnumber, latitude, longitude)   # Create the relocation entry
+                # Both are specified, so we use them
+            except:
+                if row['address']:
+                    # No coordinates provided but an address is here; use it.
+                    gres = gmaps.geocode(row['address']) # Includes city, state, zip, country if needed
+                    reloinfo = myclub(clubnumber, club.clubname, club.place, club.address, club.city, club.state, club.zip, club.country, 0.0, 0.0)
+                    reloinfo.process(gres)
+                    Relocate(clubnumber, reloinfo.latitude, reloinfo.longitude)
     data.close()
     
 
