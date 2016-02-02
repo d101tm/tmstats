7010a29474e5fe895da2083c835b55a6cdabd5dc
David Singer <david@dsinger.com>
2016-02-02 15:07:42 -0800

    Combine the functions of 'export.py' into grouper.py.

--- grouper.py
+++ grouper.py
@@ -58,24 +58,24 @@ clubs = {}
 
 class myclub:
     
-    def __init__(self, clubname, clubnumber, latitude, longitude, place, address, city, state, zip, country, area, division):
-        clubnumber = '%s' % clubnumber
-        self.clubname = clubname
-        self.clubnumber = clubnumber 
-        self.latitude = latitude
-        self.longitude = longitude
-        self.place = place
-        self.address = address
-        self.city = city
-        self.state = state
-        self.zip = zip
-        self.country = country
+    fields = ['clubnumber', 'clubname', 'latitude', 'longitude', 'place', 'address', 'city', 'state', 'zip', 'country', 'area', 'division', 'meetingday', 'meetingtime', 'color', 'goalsmet', 'activemembers']
+    
+    outfields = ['clubnumber', 'clubname', 'oldarea', 'newarea', 'color', 'goalsmet', 'activemembers', 'meetingday', 'meetingtime', 'place', 'address', 'city', 'state', 'zip', 'country',  'latitude', 'longitude', ]
+    
+    def __init__(self, *args):
+        # Assign values
+        for (f, v) in zip(self.fields, args):
+            self.__dict__[f] = v
+        # Fix up clubnumber
+        self.clubnumber = '%s' % self.clubnumber
         self.distances = []
-        self.area = area
-        self.division = division
-        clubs[clubnumber] = self
-        if latitude == 0.0 or longitude == 0.0:
-            print clubname
+        self.oldarea = self.division + self.area
+        clubs[self.clubnumber] = self
+        if self.latitude == 0.0 or self.longitude == 0.0:
+            print self.clubname, self.clubnumber, 'has no location assigned.'
+            
+    def out(self):
+        return ['%s' % self.__dict__[f] for f in self.outfields]
         
         
 ### Insert classes and functions here.  The main program begins in the "if" statement below.
@@ -108,12 +108,19 @@ if __name__ == "__main__":
     from makemap import Bounds
     bounds = Bounds()
 
+    # Get data from clubs
     curs.execute('SELECT MAX(lastdate) FROM CLUBS')
     lastdate = curs.fetchone()[0]
-
-    curs.execute('SELECT g.clubname, g.clubnumber, g.latitude, g.longitude, g.place, g.address, g.city, g.state, g.zip, g.country, c.area, c.division FROM geo g INNER JOIN clubs c on g.clubnumber = c.clubnumber WHERE c.division IN ("A", "B", "F", "G", "J") AND c.city != "Palo Alto" AND c.lastdate = %s', (lastdate,))
+    whereclause = 'WHERE c.division IN ("A", "B", "F", "G", "J") AND c.city != "Palo Alto" AND c.lastdate = "%s"' % lastdate
+    c2 = conn.cursor()
+    curs.execute('SELECT g.clubnumber, g.clubname, g.latitude, g.longitude, g.place, g.address, g.city, g.state, g.zip, g.country, c.area, c.division, c.meetingday, c.meetingtime FROM geo g INNER JOIN clubs c on g.clubnumber = c.clubnumber ' + whereclause)
     for row in curs.fetchall():
+        c2.execute('SELECT color, goalsmet, activemembers FROM clubperf WHERE entrytype = "L" AND clubnumber = %s', (row[0],))
+        row = [cell for cell in row] + [cell for cell in c2.fetchone()]
         myclub(*row)
+        
+        
+    # Now, get the performance metrics of interest
 
         
     # If there are overrides to club positioning, handle them now
@@ -150,39 +157,40 @@ if __name__ == "__main__":
          
 
     
-    groups = []
-    groupmax = math.floor(len(away) / 5)
-    print groupmax
+    divs = []
+    divmax = math.floor(len(away) / 5)
+    print divmax
     print len(away), 'clubs'
     # Now, we start clustering.  
     while away:
         c = clubs[away.pop(0)]
         print c.clubname
         c.distances.sort(key=lambda l:l[1])
-        group = [c.clubnumber]   # Start with THIS club
-        while len(group) < groupmax and len(c.distances) > 0:
+        div = [c.clubnumber]   # Start with THIS club
+        while len(div) < divmax and len(c.distances) > 0:
             (cnum, dist) = c.distances.pop(0)  # Take off the first item
             if cnum in away:
-                print "club %s is %s away from %s" % (clubs[cnum].clubname, dist, c.clubname)
+                #print "club %s is %s away from %s" % (clubs[cnum].clubname, dist, c.clubname)
                 if (dist > 50):
                     print "too far!"
                     break  # and we're done with this one
-                group.append(cnum)
+                div.append(cnum)
                 del away[away.index(cnum)]
-                print len(away), 'clubs left in away'
-        groups.append(group)
-        print len(group), 'clubs in group;', len(away), 'left to look at'        
+                #print len(away), 'clubs left in away'
+        divs.append(div)
+        print len(div), 'clubs in div', len(divs) + 1, '. ', len(away), 'left to look at'        
     
     divletters = ['','B','G','J','F','C']
+    print len(divs), 'divscreated.'
     
-    outfile = open('grouped.csv', 'wb')
+    outfile = open('dived.csv', 'wb')
     writer = csv.writer(outfile)
-    writer.writerow(['newdiv', 'newarea', 'clubnumber', 'clubname', 'oldarea', 'address', 'city', 'state', 'zip', 'latitude', 'longitude' ])
-    for i in xrange(len(groups)):
-        groupnum = i+1
-        for cnum in groups[i]:
+    writer.writerow(myclub.outfields)
+    for i in xrange(len(divs)):
+        divnum = i+1
+        for cnum in divs[i]:
             c = clubs[cnum]
-            
-            writer.writerow((divletters[groupnum], 1, cnum, c.clubname, '%s%s' % (c.division, c.area), c.address, c.city, c.state, c.zip, c.latitude, c.longitude))
+            c.newarea = '%s%d' % (divletters[divnum], 1)
+            writer.writerow(c.out())
     outfile.close()
     
