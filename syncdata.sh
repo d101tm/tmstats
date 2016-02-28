e374e4095e669e27067d4f72337edc6b9cdfbdf5
David Singer <david@dsinger.com>
2016-02-27 17:38:05 -0800

    Don't synch the YML files.

--- syncdata.sh
+++ syncdata.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -vtzr --exclude 'build' d4tm.org:bin/tmstats/data/ ~/src/tmstats/data/
+rsync -vtzr --exclude 'build' --exclude '*.yml' d4tm.org:bin/tmstats/data/ ~/src/tmstats/data/
 ssh d4tm.org 'mysqldump d4tmadmn_tmstats | gzip' | gunzip | mysql d4tmadmn_tmstats
