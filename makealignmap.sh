7c7fd1e6ba1dc2254680f738a4721b7ef0ca6104
David Singer <david@dsinger.com>
2016-03-01 15:53:34 -0800

    Use new names

--- makealignmap.sh
+++ makealignmap.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-./alignmap.py --pindir /Users/david/src/map/pins --district 101 && open ../map/align.htm; (cd data;../allstats.py --proforma grouped.csv) && open data/stats.html
-scp data/stats.html d4tm.org:www/files/stats/proforma101.html
-scp data/newmarkers.js d4tm.org:www/images/map
-ssh d4tm.org bin/tmstats/clearcache.sh
+./alignmap.py --pindir /Users/david/src/map/pins --district 101 --testalign d101align.csv  --makedivisions && (cd data;../allstats.py --proforma d101align.csv)
+if [[ $? ]] ; then
+    scp data/d101proforma.html d4tm.org:www/files/stats
+    scp data/d101newmarkers.js d4tm.org:www/images/map
+    ssh d4tm.org bin/tmstats/clearcache.sh
+fi
