f7e03c9f8e4817e6c9e1181303f683e4eb24b1ed
David Singer <david@dsinger.com>
2016-03-01 15:48:30 -0800

    Allow using the same program for D4 and D101
    * Change filename to a parameter, or create from District number
    * Allow making division boundaries or not, default based on District
    * Only add District border for District 101

--- alignmap.py
+++ alignmap.py
@@ -11,7 +11,6 @@ from geometry import convexHull
 import pytess
 from shapely.ops import cascaded_union
 from shapely.geometry import Polygon
-from d101 import d101
 
 def inform(*args, **kwargs):
     """ Print information to 'file', depending on the verbosity level. 
@@ -34,17 +33,30 @@ if __name__ == "__main__":
     parms = tmparms.tmparms()
     parms.add_argument('--quiet', '-q', action='count', default=0)
     parms.add_argument('--verbose', '-v', action='count', default=0)
-    parms.add_argument('--outfile', dest='outfile', default='newmarkers.js')
+    parms.add_argument('--outfile', dest='outfile', default=None)
     parms.add_argument('--newAlignment', dest='newAlignment', default=None, help='Overrides area/division data from the CLUBS table.')
     parms.add_argument('--pindir', dest='pindir', default=None, help='Directory with pins; default uses Google pins')
     parms.add_argument('--mapoverride', dest='mapoverride', default=None, help='Google spreadsheet with overriding address and coordinate information')
-    parms.add_argument('--testalign', dest='testalign', default='grouped.csv')
+    parms.add_argument('--testalign', dest='testalign', default=None)
     parms.add_argument('--district', dest='district')
+    parms.add_argument('--makedivisions', dest='makedivisions', action='store_true')
+    parms.add_argument('--nomakedivisions', dest='nomakedivisions', action='store_true')
     # Add other parameters here
     parms.parse() 
     
     # Compute verbosity level.  Default is zero.
     parms.verbosity = parms.verbose - parms.quiet
+    
+    # Make choices for unspecified options depending on the District
+    if parms.district == '4':
+        if parms.makedivisions or parms.nomakedivisions:
+            parms.makedivisions = parms.makedivisions
+    elif parms.district == '101':
+        if parms.makedivisions or parms.nomakedivisions:
+            parms.makedivisions = not parms.nomakedivisions
+    if not parms.outfile:
+        parms.outfile = 'd%snewmarkers.js' % parms.district
+
 
     
     # Promote information from parms.makemap if not already specified
@@ -96,7 +108,13 @@ if __name__ == "__main__":
             newdivs[newdiv].append(coords)
         for c in clubs.keys():
             if c not in newclubs:
-                del clubs[c]    
+                del clubs[c]  
+        for c in clubs.keys():  
+            try:
+                clubs[c].color
+            except AttributeError:
+                print '%s (%s) does not have a color' % (clubs[c].clubname, c)
+                clubs[c].color = ''
 
     
 
@@ -106,97 +124,100 @@ if __name__ == "__main__":
     parms.showdetails = True
     makemap(outfile, clubs, parms)
     
-    outfile.write("""
+    if parms.makedivisions:
+    
+        outfile.write("""
     
     
-    function DrawHull(hullPoints, color) {
-         polyline = new google.maps.Polygon({
-          map: map,
-          paths:hullPoints, 
-          fillColor:color,
-          strokeWidth:2, 
-          fillOpacity:0.5, 
-          strokeColor:color,
-          strokeOpacity:0.7
-         })
-         };
-    """)
+        function DrawHull(hullPoints, color) {
+             polyline = new google.maps.Polygon({
+              map: map,
+              paths:hullPoints, 
+              fillColor:color,
+              strokeWidth:2, 
+              fillOpacity:0.5, 
+              strokeColor:color,
+              strokeOpacity:0.7
+             })
+             };
+        """)
     
-    # Now, compute the new divisions.
-    d101 = Polygon(d101)
+        # Now, compute the new divisions.
+        from d101 import d101
+        d101 = Polygon(d101)
     
-    sites = {}         # (lat,lng) ==> Division
-    for c in clubs.values():
-        point = (c.latitude, c.longitude)
-        if point in sites:
-            if c.division != sites[point]:
-                sites[point] += c.division
-        else:
-            sites[point] = c.division
+        sites = {}         # (lat,lng) ==> Division
+        for c in clubs.values():
+            point = (c.latitude, c.longitude)
+            if point in sites:
+                if c.division != sites[point]:
+                    sites[point] += c.division
+            else:
+                sites[point] = c.division
             
-    points = [loc for loc in sites.keys() if len(sites[loc]) == 1]
-    voronoipolys = pytess.voronoi(points, buffer_percent=200)
+        points = [loc for loc in sites.keys() if len(sites[loc]) == 1]
+        voronoipolys = pytess.voronoi(points, buffer_percent=200)
            
 
-    # Compute the union of polygons for each division and write it to the file
-    polygons = {}
-    for (point, poly) in voronoipolys:
-        if point in sites:
-            div = sites[point]
-            if div not in polygons:
-                polygons[div] = []
-            polygons[div].append(Polygon(poly))
+        # Compute the union of polygons for each division and write it to the file
+        polygons = {}
+        for (point, poly) in voronoipolys:
+            if point in sites:
+                div = sites[point]
+                if div not in polygons:
+                    polygons[div] = []
+                polygons[div].append(Polygon(poly))
             
-    def dopoly(outfile, outline, div, num=0):
-        if num > 0:
-            varname = '%s%d' % (div, num)
-        else:
-            varname = div 
+        def dopoly(outfile, outline, div, num=0):
+            if num > 0:
+                varname = '%s%d' % (div, num)
+            else:
+                varname = div 
+            outfile.write("""
+            var div%s = [
+            """ % varname)
+            outfile.write(',\n'.join(['new g.LatLng(%s, %s)' % p for p in outline.exterior.coords]))
+            outfile.write("""];
+            DrawHull(div%s, fillcolors["%s"]);
+            """ % (varname,d))
+            
+        for d in polygons:
+            outline = polygons[d][0]
+            for p in polygons[d][1:]:
+                outline = outline.union(p)
+            outline = outline.intersection(d101)
+
+            if outline.type == 'Polygon':
+                dopoly(outfile, outline, d)
+            else:
+                 num = 0
+                 for poly in outline.geoms:
+                     num += 1
+                     dopoly(outfile, poly, d, num)
+
+        # Finally, draw the D101 boundary
+    
         outfile.write("""
-        var div%s = [
-        """ % varname)
-        outfile.write(',\n'.join(['new g.LatLng(%s, %s)' % p for p in outline.exterior.coords]))
+    
+    
+        function DrawBoundary(hullPoints) {
+             polyline = new google.maps.Polygon({
+              map: map,
+              paths:hullPoints, 
+              fillColor:'#FFFFC0',
+              strokeWidth:2, 
+              fillOpacity:0.2, 
+              strokeColor:'#000000',
+              strokeOpacity:0.7
+             })
+             };
+
+        var d101 = [
+        """)
+        outfile.write(',\n'.join(['new g.LatLng(%s, %s)' % p for p in d101.exterior.coords]))
         outfile.write("""];
-        DrawHull(div%s, fillcolors["%s"]);
-        """ % (varname,d))
-            
-    for d in polygons:
-        outline = polygons[d][0]
-        for p in polygons[d][1:]:
-            outline = outline.union(p)
-        outline = outline.intersection(d101)
-
-        if outline.type == 'Polygon':
-            dopoly(outfile, outline, d)
-        else:
-             num = 0
-             for poly in outline.geoms:
-                 num += 1
-                 dopoly(outfile, poly, d, num)
-
-    # Finally, draw the D101 boundary
-    
-    outfile.write("""
-    
-    
-    function DrawBoundary(hullPoints) {
-         polyline = new google.maps.Polygon({
-          map: map,
-          paths:hullPoints, 
-          fillColor:'#FFFFC0',
-          strokeWidth:2, 
-          fillOpacity:0.2, 
-          strokeColor:'#000000',
-          strokeOpacity:0.7
-         })
-         };
-
-    var d101 = [
-    """)
-    outfile.write(',\n'.join(['new g.LatLng(%s, %s)' % p for p in d101.exterior.coords]))
-    outfile.write("""];
-    DrawBoundary(d101);
-    """)
+        DrawBoundary(d101);
+        """)
     
 
     outfile.close()
