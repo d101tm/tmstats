7d0330ab2e95696805d6f13c9f0ad6ae25b32f64
David Singer <david@dsinger.com>
2016-03-09 17:39:23 -0800

    Fix filenames.

--- makealignmap.sh
+++ makealignmap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./alignmap.py --pindir /Users/david/src/map/pins --district 101 --testalign d101align.csv  --makedivisions && (cd data;../allstats.py --proforma d101align.csv)
+./alignmap.py --pindir /Users/david/src/map/pins --district 101 --testalign d101align.csv  --makedivisions && (cd data;../allstats.py --outfile d101proforma.html --proforma d101align.csv)
 if [[ $? ]] ; then
     scp data/d101proforma.html d4tm.org:www/files/stats
     scp data/d101newmarkers.js d4tm.org:www/images/map
