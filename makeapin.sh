9dc024be6aba3ce9a57948f569dc0978c8c9d103
David Singer <webmaster@d101tm.org>
2016-07-18 15:08:36 -0700

    Name the new pin to match its content

--- makeapin.sh
+++ makeapin.sh
@@ -2,4 +2,4 @@
 
 # This makes one pin.  
 
-convert empty.png -quality 100 -font Helvetica-Bold -pointsize 11   -fill '#a38aff' -draw 'rectangle 1,1 18,10' -fill black  -draw "gravity Center text 1,-2 'G6'"  test.png && open test.png
+convert empty.png -quality 100 -font Helvetica-Bold -pointsize 11   -fill '#a38aff' -draw 'rectangle 1,1 18,10' -fill black  -draw "gravity Center text 1,-2 '$1'"  $1.png 
