6837d8225a63451c9c3e89a2496879a0e3e88187
David Singer <webmaster@d101tm.org>
2016-07-20 20:51:01 -0700

    Add clubs-by-area processing to the cycle

--- tonight.sh
+++ tonight.sh
@@ -6,6 +6,7 @@ cd data
 ../makelocationreport.py --color --infile d101align.csv
 ../listclubsbycity.py  --newAlignment d101align.csv
 ../makealignmentpage.py > d101index.html
+../build101areapage.py --outfile areapage.html --newAlignment d101align.csv
 
 if [[ $? ]] ; then 
     if [[ "block15" == $(hostname) ]] ; then
@@ -18,6 +19,7 @@ if [[ $? ]] ; then
         cp d101location.html ~/map
         cp d101index.html ~/files/alignment/index.html
         cp clublist.html ~/files/reports/clubs-by-city/index.html
+        cp areapage.html ~/files/reports/areapage.html
     else
         echo "Copying to d101tm.org"
         scp d101proforma.html d101tm.org:files/alignment/
