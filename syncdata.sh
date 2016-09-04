91f446fc303053ecf00a26150698f4eced135509
David Singer <david@dsinger.com>
2016-09-03 22:31:15 -0700

    Synchronize from D101, not D4

--- syncdata.sh
+++ syncdata.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-rsync -vtzr --exclude 'build' --exclude '*.yml' d4tm.org:bin/tmstats/data/ ~/src/tmstats/data/
-ssh d4tm.org 'mysqldump d4tmadmn_tmstats | gzip' | gunzip | mysql d4tmadmn_tmstats
+rsync -vtzrL --exclude 'build' --exclude '*.yml' d101tm.org:src/tmstats/data/ ~/src/tmstats/data/
+ssh d101tm.org 'mysqldump d101tm_tmstats | gzip' | gunzip | mysql d101tm_tmstats
+ssh d101tm.org 'mysqldump d101tm_org | gzip' | gunzip | mysql d101tm_org
