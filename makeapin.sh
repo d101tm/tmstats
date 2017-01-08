0ba99f2c58df6e0efa814be09cc2df7185e55092
David Singer <david@dsinger.com>
2017-01-07 20:01:55 -0800

    get closer to d101 style

--- makeapin.sh
+++ makeapin.sh
@@ -2,4 +2,4 @@
 
 # This makes one pin.  
 
-convert empty.png -quality 100 -font Helvetica-Bold -pointsize 11   -fill '#a38aff' -draw 'rectangle 1,1 18,10' -fill black  -draw "gravity Center text 1,-2 '$1'"  $1.png 
+convert empty.png -quality 100 -font Helvetica-Bold -pointsize 11   -fill '#a38aff' -draw 'rectangle 1,1 9,10' -fill '#B0E3F2' -draw 'rectangle 10,1 18,10' -fill black  -draw "gravity Center text 1,-2 '$1'"  $1.png 
