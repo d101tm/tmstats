c78153628b1c04c36f0a004f4d6e476c49e33297
David Singer <david@dsinger.com>
2017-02-23 19:33:56 -0800

    Create maps for each proposed area

--- makelocationreport.py
+++ makelocationreport.py
@@ -4,7 +4,7 @@
 
 import dbconn, tmutil, sys, os, csv, re
 from tmutil import distance_on_unit_sphere 
-
+from makemap import Bounds
 
 def inform(*args, **kwargs):
     """ Print information to 'file' unless suppressed by the -quiet option.
@@ -31,14 +31,35 @@ def computeMaxDistance(locations):
                 maxc2 = c2
     return (maxd, maxc1, maxc2)
     
+    
+def makeareamap(area, locations):
+
+    b = Bounds()
+    mapbase="https://maps.googleapis.com/maps/api/staticmap?key=" + parms.staticmapsapikey + "&"
+    mapparts = []
+
+    clubinfo = []
+    marker = 'A'
+    for (clubname, latitude, longitude) in locations:
+        b.extend(latitude, longitude)
+        mapparts.append('markers=label:%s%%7C%s,%s' % (marker, latitude, longitude))
+        marker = chr(ord(marker)+1)
+    
+    
+
+ #   mapparts.append("center=%s" % (b.centercoords()))
+    mapparts.append("size=640x640&scale=2")   # As large as possible, at least for now
+    return '%s%s' % (mapbase, '&'.join(mapparts))
 
 def openarea(outfile, area, color):
     ret = []
-    ret.append('<div class="area">\n')
+    ret.append('<div class="area clearfix">\n')
     ret.append('<h4>Area %s (%%.2f miles between %%s and %%s)</h4>\n' % area)
+    ret.append('<div class="areainfo">')
     ret.append('<table class="areatable">\n')
     ret.append('<thead>\n')
     ret.append('<tr>\n')
+    ret.append('<th class="marker">ID</th>')
     ret.append('<th class="cnum">Number</th><th class="cname">Name</th>')
     if color:
         ret.append('<th class="color">Color</th>')
@@ -48,13 +69,20 @@ def openarea(outfile, area, color):
     return ret
     
 def closearea(outfile, text, locations):
+    # This is gross and relies on knowing what "openarea" does.
+    
     if len(locations) > 1:
-        outfile.write(''.join(text) % computeMaxDistance(locations))
+        text[1] = text[1] % computeMaxDistance(locations)
+        outfile.write(''.join(text))
     else:
-        # This is gross and relies on knowing what "openarea" does.
         text[1] = re.sub(r'\(.*\)', '', text[1])  # Remove distance
         outfile.write(''.join(text))
     outfile.write('</tbody></table>\n')
+    outfile.write('</div>\n')
+    if len(locations) > 1:
+        outfile.write('<div class="areamap">')
+        outfile.write('<img src="%s" width="640px">' % makeareamap(text[1].split()[2], locations))
+        outfile.write('</div>')
     outfile.write('</div>\n')
     
 def opendiv(outfile, division):
@@ -78,6 +106,8 @@ if __name__ == "__main__":
     parms.add_argument('--infile', default='d101align.csv')
     parms.add_argument('--outfile', default='d101location.html')
     parms.add_argument('--color', action='store_true')
+    parms.add_argument('--mapdir', default=None, help='Directory to use for the area map files.')
+    
     # Add other parameters here
     parms.parse() 
    
@@ -97,7 +127,7 @@ if __name__ == "__main__":
      body {font-family: Arial }
     
 
-    .area {margin-bottom: 12pt; width: 100%; page-break-inside: avoid; display: block;}
+    .area {margin-bottom: 12pt; width: 100%; page-break-inside: avoid; display: block; clear: both;}
 
     .division {border: none; break-before: always !important; display: block; float: none; position: relative; page-break-inside: avoid; page-break-after: always !important;}
       .areatable {font-size: 10pt; border: 1px solid black; border-collapse: collapse;}
@@ -108,6 +138,7 @@ if __name__ == "__main__":
       .cnum {text-align: right; width: 7%;}
       .cname {text-align: left; width: 15%;}
       .color {text-align: left; width: 5%;}
+      .marker {text-align: left; width: 5%; font-weight: bold;}
       .red {background-color: red;}
       .yellow {background-color: yellow;}
       .green {background-color: green;}
@@ -115,6 +146,25 @@ if __name__ == "__main__":
       .members {text-align: right; width: 5%;}
       .mtg {text-align: left; width: 25%;}
       .loc {text-align: left; width: 30%;}
+      
+      .area {
+          width: 95%;
+          margin: auto;
+          padding: 10px;
+      }
+      .areainfo {
+          width: 45%;
+          float: left;
+      }
+      .areamap {
+          margin-left: 50%;
+      }
+      
+      .clearfix:after {
+        content: "";
+        display: table;
+        clear: both;
+      }
     </style>
     </head>
     <body>
@@ -135,11 +185,13 @@ if __name__ == "__main__":
             opendiv(outfile, div)
         if thisarea != area:
             accum = openarea(outfile, area, parms.color)
+            marker = 'A'
         thisarea = area
         thisdiv = div
         outrow = []
         row['closing'] = '<br />(Probably closing)' if row['likelytoclose'] else ''
         outrow.append('<tr class="myrow%s">' % (' ghost' if row['likelytoclose'] else ''))
+        outrow.append('  <td class="marker">%s</marker>' % marker)
         outrow.append('  <td class="cnum">{clubnumber}</td><td class="cname">{clubname}{closing}</td>')
         if parms.color:
             outrow.append('  <td class="color {color}">{color}</td>\n')
@@ -150,8 +202,13 @@ if __name__ == "__main__":
         outrow.append('</tr>')
         accum.append(('\n'.join(outrow)).format(**row))
         locations.append((row['clubname'], row['latitude'], row['longitude']))
+        marker = chr(ord(marker)+1)
         
     closearea(outfile, accum, locations)
     closediv(outfile)
     outfile.write("</body></html>\n")
     outfile.close()
+    
+
+
+
