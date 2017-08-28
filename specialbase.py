2e071dc529ad095da0d326dc49fdc15f4a56df6b
David Singer <david@dsinger.com>
2017-08-28 16:51:11 -0700

    Move 'special base' handling into its own module

---
+++ specialbase.py
@@ -0,0 +1,6 @@
+# In case WHQ messes up the base membership for clubs, we can override it here.  
+# Programs which use the base membership must import specials from this routine.
+# specials is a directory of clubname/base pairs, like so:
+specials = {1:14, 2:10}
+# normally, though, there aren't any overrides, so it's empty:
+specials = {}
