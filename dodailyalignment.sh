4eeb037d5c13bb7776701826d5fe95e2ee458818
David Singer <webmaster@d101tm.org>
2017-09-09 15:55:26 -0700

    Clear the cache on update.

--- dodailyalignment.sh
+++ dodailyalignment.sh
@@ -9,6 +9,7 @@ if [[ $? ]] ; then
         echo "Copying map and reports"
         cp clublist.html ~/files/reports/clubs-by-city/index.html
         cp areapage.html ~/files/reports/areapage.html
+        ../clearcache.py divisions-and-areas
         cp d101newmarkers.js ~/map/
     fi
 
