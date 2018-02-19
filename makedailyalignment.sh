59042ac53ec2d16f131afc8a56c3cf5b9915125b
David Singer <david@dsinger.com>
2018-02-19 15:02:38 -0800

    Use a subdirectory for working files
    Also allow the script to be run from the main
      directory or the data directory.

--- makedailyalignment.sh
+++ makedailyalignment.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
+# Even if we're running in a weird shell, let's use THIS directory as the current directory
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+data="$SCRIPTPATH/data"
+cd "$data"   # Run in the data directory.
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/d101tm/lib/lib
-../createalignment.py
-../alignmap.py --pindir pins --district 101 --testalign d101align.csv --makedivisions
-../allstats.py --outfile d101proforma.html --testalign d101align.csv 
-../makelocationreport.py --color --infile d101align.csv
-../clubchanges.py --from 3/15 --to 5/20 --outfile ~/files/dailyalignment/changes.html
-../makealignmentpage.py > d101index.html
+
+# Make a subdirectory for all of the products of this run
+mkdir alignment 2>/dev/null
+export workfile=alignment/d101align.csv
+echo createalignment
+../createalignment.py --outfile $workfile
+echo alignmap 
+../alignmap.py --pindir pins --district 101 --testalign $workfile --makedivisions --outdir alignment
+echo allstats
+../allstats.py --outfile d101proforma.html --testalign $workfile --outdir alignment
+echo makelocationreport
+../makelocationreport.py --color --infile $workfile --outdir alignment
+echo clubchanges
+../clubchanges.py --from 3/15 --to 5/20 --outfile alignment/changes.html
+echo makealignmentpage
+../makealignmentpage.py > alignment/d101index.html
 
 if [[ "block15" == $(hostname) || "ps590973" == $(hostname) ]] ; then
         echo "Copying to dailyalignment"
-        cp d101proforma.html ~/files/dailyalignment
-        cp d101newmarkers.js ~/files/dailyalignment
-        cp d101location.html ~/files/dailyalignment
-        cp d101minimal.html ~/files/dailyalignment
-        cp d101migration.html ~/files/dailyalignment
-        cp d101index.html ~/files/dailyalignment/index.html
-
+        cp alignment/ ~/files/dailyalignment
 fi
