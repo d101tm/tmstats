48d670a333fc1fb68aea0442e64544ce21f68f3a
David Singer <david@dsinger.com>
2018-03-15 16:08:09 -0700

    9 goals suffice unless --requiremembership
    If --requiremembership is specified, clubs need 9 goals
      and they must meet the membership criteria for
      President's Distinguished (or, once WHQ is reporting
      on Distinguished clubs, they must be officially PD).

--- presidentsclub.py
+++ presidentsclub.py
@@ -21,7 +21,8 @@ if __name__ == "__main__":
     parms.add_argument('--quiet', '-q', action='count')
     parms.parser.add_argument("--finaldate", dest='finaldate', type=str, default='4/15')
     parms.parser.add_argument('--outfile', default='presidentsclub.txt')
-    parms.parser.add_argument('--earning', default='$100 in District Credit')
+    parms.parser.add_argument('--earning', default='$101 in District Credit')
+    parms.parser.add_argument('--requiremembership', action='store_true', help='Specify to require that clubs meet membership goals to qualify.')
 
     # Do global setup
     globals.setup(parms)
@@ -44,21 +45,28 @@ if __name__ == "__main__":
 
     winners = []
     
-    # See if WHQ has posted any President's Distinguished Clubs; if so,
-    #   use their information.  If not, calculate on our own.
+    if parms.requiremembership:
+    
+        # See if WHQ has posted any President's Distinguished Clubs; if so,
+        #   use their information.  If not, calculate on our own.
 
-    curs.execute("SELECT clubnumber, clubname FROM clubperf WHERE clubdistinguishedstatus = 'P' AND asof = %s", (targetdate,))
+        curs.execute("SELECT clubnumber, clubname FROM clubperf WHERE clubdistinguishedstatus = 'P' AND asof = %s", (targetdate,))
 
-    for c in curs.fetchall():
-        winners.append(myclub(*c))
-
-    if not winners:
-        print >> sys.stderr, 'No President\'s Distinguished Clubs from WHQ.'
-        # Either WHQ hasn't posted or no one qualifies.  Use our calculation.
-        curs.execute("SELECT c.clubnumber, c.clubname FROM clubperf c INNER JOIN distperf d ON c.clubnumber = d.clubnumber AND c.entrytype = 'L' AND d.entrytype = 'L' AND ((d.aprrenewals > 20) OR (d.aprrenewals - c.membase) >= 5) WHERE c.goalsmet >= 9")
         for c in curs.fetchall():
             winners.append(myclub(*c))
 
+        if not winners:
+            print >> sys.stderr, 'No President\'s Distinguished Clubs from WHQ.'
+            # Either WHQ hasn't posted or no one qualifies.  Use our calculation.
+            curs.execute("SELECT c.clubnumber, c.clubname FROM clubperf c INNER JOIN distperf d ON c.clubnumber = d.clubnumber AND c.entrytype = 'L' AND d.entrytype = 'L' AND ((d.aprrenewals > 20) OR (d.aprrenewals - c.membase) >= 5) WHERE c.goalsmet >= 9")
+            for c in curs.fetchall():
+                winners.append(myclub(*c))
+
+    else:
+        # We only care about clubs with at least 9 goals.
+        curs.execute("SELECT clubnumber, clubname FROM clubperf WHERE goalsmet >= 9 AND asof = %s", (targetdate,))
+        for c in curs.fetchall():
+            winners.append(myclub(*c))
 
     if len(winners) > 0:
         outfile.write('<p><b>Congratulations</b> to %s for earning %s!</p>' % (getClubBlock(winners), parms.earning))
