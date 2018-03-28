ae867f9bf1c03dc8cbffa50e8addcdb7c4a39de4
David Singer <david@dsinger.com>
2018-03-28 16:19:46 -0700

    Hardcode pathids, don't compute them
    Also, include old pathids and mappings for the fixup

--- awardinfo.py
+++ awardinfo.py
@@ -1,67 +1,78 @@
 class Awardinfo:
-        oldawardnames = {'CC':'Competent Communicator',
-                          'ACB': 'Advanced Communicator Bronze',
-                          'ACS': 'Advanced Communicator Silver',
-                          'ACG': 'Advanced Communicator Gold',
-                          'CL': 'Competent Leader',
-                          'ALB': 'Advanced Leader Bronze',
-                          'ALS': 'Advanced Leader Silver',
-                          'LDREXC': 'High Performance Leadership Project',
-                          'DTM': 'Distinguished Toastmaster'}
-                          
-        pathnames = {'Dynamic Leadership',
-                          'Effective Coaching',
-                          'Innovative Planning',
-                          'Leadership Development',
-                          'Motivational Strategies',
-                          'Persuasive Influence',
-                          'Presentation Mastery',
-                          'Strategic Relationships',
-                          'Team Collaboration',
-                          'Visionary Communication'}
-                         
-                         
-        # The names of the awards for paths are derived from the path name:
-        #  First three letters of the first two words in the path (uppercased)
-        #  Followed by "L" and the level number.
-        #  Compute them all to reduce typing.
-        pathids = {}
-        for p in pathnames:
-            id = ''.join([s[0:3].upper() for s in p.split()[:2]])
-            pathids[id] = p
-        
-        levels = {}
-        paths = {}
-        lookup = oldawardnames
-        for item in oldawardnames:
-            levels[item] = 0
-            
-        for p in pathids:
-            for l in [1, 2, 3, 4, 5]:
-                index = '%sL%d' % (p, l)
-                lookup[index] = '%s Level %d' % (pathids[p], l)
-                levels[index] = l
-                paths[index] = pathids[p]
+    oldawardnames = {'CC': 'Competent Communicator',
+                     'ACB': 'Advanced Communicator Bronze',
+                     'ACS': 'Advanced Communicator Silver',
+                     'ACG': 'Advanced Communicator Gold',
+                     'CL': 'Competent Leader',
+                     'ALB': 'Advanced Leader Bronze',
+                     'ALS': 'Advanced Leader Silver',
+                     'LDREXC': 'High Performance Leadership Project',
+                     'DTM': 'Distinguished Toastmaster'}
 
-if __name__ == '__main__':
-    from pprint import pprint
-    new = {}
-    old = {}
-    newtoold = {}
-    oldtonew = {}
-    for p in Awardinfo().pathnames:
-        w = p.upper().split()[:2]
-        n = w[0][0] + w[1][0]
-        o = w[0][0:3] + w[1][0:3]
-        new[n] = p
-        old[o] = p
-        newtoold[n] = o
-        oldtonew[o] = n
-    print 'pathids = ',
-    pprint(new)
-    print 'oldpathids = ',
-    pprint(old)
-    print 'newtoold = ', 
-    pprint(newtoold)
-    print 'oldtonew = ',
-    pprint(oldtonew)
+    pathnames = {'Dynamic Leadership',
+                 'Effective Coaching',
+                 'Innovative Planning',
+                 'Leadership Development',
+                 'Motivational Strategies',
+                 'Persuasive Influence',
+                 'Presentation Mastery',
+                 'Strategic Relationships',
+                 'Team Collaboration',
+                 'Visionary Communication'}
+
+    pathids = {'DL': 'Dynamic Leadership',
+               'EC': 'Effective Coaching',
+               'IP': 'Innovative Planning',
+               'LD': 'Leadership Development',
+               'MS': 'Motivational Strategies',
+               'PI': 'Persuasive Influence',
+               'PM': 'Presentation Mastery',
+               'SR': 'Strategic Relationships',
+               'TC': 'Team Collaboration',
+               'VC': 'Visionary Communication'}
+
+    oldpathids = {'DYNLEA': 'Dynamic Leadership',
+                  'EFFCOA': 'Effective Coaching',
+                  'INNPLA': 'Innovative Planning',
+                  'LEADEV': 'Leadership Development',
+                  'MOTSTR': 'Motivational Strategies',
+                  'PERINF': 'Persuasive Influence',
+                  'PREMAS': 'Presentation Mastery',
+                  'STRREL': 'Strategic Relationships',
+                  'TEACOL': 'Team Collaboration',
+                  'VISCOM': 'Visionary Communication'}
+
+    newtoold = {'DL': 'DYNLEA',
+                'EC': 'EFFCOA',
+                'IP': 'INNPLA',
+                'LD': 'LEADEV',
+                'MS': 'MOTSTR',
+                'PI': 'PERINF',
+                'PM': 'PREMAS',
+                'SR': 'STRREL',
+                'TC': 'TEACOL',
+                'VC': 'VISCOM'}
+
+    oldtonew = {'DYNLEA': 'DL',
+                'EFFCOA': 'EC',
+                'INNPLA': 'IP',
+                'LEADEV': 'LD',
+                'MOTSTR': 'MS',
+                'PERINF': 'PI',
+                'PREMAS': 'PM',
+                'STRREL': 'SR',
+                'TEACOL': 'TC',
+                'VISCOM': 'VC'}
+
+    levels = {}
+    paths = {}
+    lookup = oldawardnames
+    for item in oldawardnames:
+        levels[item] = 0
+
+    for p in pathids:
+        for l in [1, 2, 3, 4, 5]:
+            index = '%sL%d' % (p, l)
+            lookup[index] = '%s Level %d' % (pathids[p], l)
+            levels[index] = l
+            paths[index] = pathids[p]
