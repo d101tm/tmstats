78a424f1925db6d6885714e64bfb5967d6323beb
David Singer <david@dsinger.com>
2018-08-10 21:56:19 -0700

    Python3 fixes: *   Replace tabs with spaces throughout; remove trailing whitespace *   Set default for the -v argument to 0 *   Open the eventual output file as bytes, since Dropbox will give us bytes

--- getfromdropbox.py
+++ getfromdropbox.py
@@ -23,16 +23,16 @@ class Output:
         self.error:  An error message if errors occur (currently only in authentication)
         self.warning:  A warning if no file is found
     """
-
-
+    
+    
     def __init__(self):
         self.cursor = None
         self.filename = None
         self.filetime = None
         self.file = None
         self.error = None
-	self.warning = None
-
+        self.warning = None
+    
     def __repr__(self):
         return 'cursor: %s\nfilename: %s\nfiletime: %s\nerror: %s\n\n%s' % (self.cursor, self.filename, self.filetime, self.error, self.content)
 
@@ -41,52 +41,52 @@ def getDropboxFile(token, directory, extensions, cursor=None):
         directory is the path to examine (ignored if cursor is not None)
         extensions is a list of extensions to consider; empty means any extension
         cursor is the state cursor from a previous execution; if omitted, examines all files
-
+        
         Returns an object of class Output """
-
+    
     output = Output()
-
+    
     if not token:
         output.error = "ERROR: No access token provided; try generating one from the app console on the web."
         return output
-
+    
     dbx = dropbox.Dropbox(token)
-
+    
     # Make sure we have a proper token
     try:
         dbx.users_get_current_account()
     except AuthError as err:
         output.error = "ERROR: Invalid access token; try re-generating an access token from the app console on the web."
         return output
-
+    
     # Set up to iterate through Dropbox folder
     latestfile = None
     latesttime = datetime.min
     live = True
     fileid = None
     had_cursor = cursor
-
+    
     # Normalize extensions
     if extensions:
         extensions = ['.' + e.lower().replace('.','') for e in extensions]
-
+    
     # Normalize directory
     directory = directory.strip()
     if directory and directory[0] != '/':
         directory = '/' + directory
-
-
+    
+    
     while live:
         # Get information from Dropbox
         if cursor:
             result = dbx.files_list_folder_continue(cursor)
         else:
             result = dbx.files_list_folder(directory)
-
+        
         # Set up for next iteration if need be
         live = result.has_more
         cursor = result.cursor
-
+        
         for f in result.entries:
             path = f.path_lower
             ext = os.path.splitext(path)[1]
@@ -99,10 +99,10 @@ def getDropboxFile(token, directory, extensions, cursor=None):
                 except AttributeError as e:
                     print(e)
                     pass
-
+    
     output.cursor = cursor
-
-
+    
+    
     # If we found a file, set output appropriately
     if latestfile:
         output.filename = os.path.split(latestfile)[1]
@@ -119,17 +119,17 @@ def getDropboxFile(token, directory, extensions, cursor=None):
                     s += ','
                 s += ' OR ' + extensions[-1]
             output.warning += ' ' + s
-
+    
     return output
 
 
 if __name__ == "__main__":
     import tmparms
-
-
+    
+    
     # Handle parameters
     parms = tmparms.tmparms(description=__doc__)
-    parms.add_argument('--verbose', '-v', action='count', help="Increase verbosity of output")
+    parms.add_argument('--verbose', '-v', action='count', help="Increase verbosity of output", default=0)
     parms.add_argument('--directory', dest='directory', default='', help="Dropbox directory to examine")
     parms.add_argument('--extensions', dest='extensions', metavar="EXTENSION", type=str, nargs='+', action="append", help="extensions to consider")
     parms.add_argument('--outfile', dest='outfile', default='-', help="filename for output file; specify '-' for stdout; specify '+' to use the name of the file found; specify 'something.+' to keep the extension of the file but force the name to be 'something'.  File is not changed if no file is found.")
@@ -142,7 +142,7 @@ if __name__ == "__main__":
     
     # Do global setup
     globals.setup(parms, connect=False)
-
+    
     # Flatten extension in case it was specified many times
     if parms.extensions:
         e = []
@@ -151,10 +151,10 @@ if __name__ == "__main__":
                 e.extend(element)
             else:
                 e.append(element)
-
+        
         parms.extensions = e
-
-
+    
+    
     if parms.cfile:
         parms.cursor = None       # If the file doesn't exist yet
         try:
@@ -162,28 +162,28 @@ if __name__ == "__main__":
                 parms.cursor = f.read().strip()
         except IOError:
             pass
-
-
+    
+    
     output = getDropboxFile(parms.dropboxtoken, parms.directory, parms.extensions, parms.cursor)
-
+    
     if parms.cfile:
         with open(parms.cfile, 'w') as f:
             f.write(output.cursor)
-
-
+    
+    
     if parms.verbose >= 2:
         sys.stderr.write('cursor: %s\n' % output.cursor)
-
+    
     if output.file:
         if parms.outfile == '-' or parms.outfile.strip() == '':
-            outfile = sys.stdout
+            outfile = sys.stdout.buffer
             if parms.namefile:
                 with open(parms.namefile, 'w') as f:
                     f.write('')
         else:
             if parms.outdir:
                 parms.outdir = os.path.expanduser(parms.outdir)
-
+            
             if parms.outfile.endswith('.+'):
                 # Construct filename, preserving the extension we found
                 outfn = os.path.splitext(parms.outfile)[0] + os.path.splitext(output.filename)[1]
@@ -193,16 +193,16 @@ if __name__ == "__main__":
             else:
                 # Use the name we were given
                 outfn = parms.outfile
-
+            
             outfn = os.path.join(parms.outdir, outfn)
             # Write as an absolute filename
             if parms.namefile:
                 with open(parms.namefile, 'w') as f:
                     f.write(os.path.abspath(outfn))
-
-            outfile = open(outfn, 'w')
+            
+            outfile = open(outfn, 'wb')  
         outfile.write(output.file.read())
-
+        
         if parms.verbose:
             sys.stderr.write('Using %s, last modified at %s UTC\n' % (output.filename, output.filetime))
     else:
