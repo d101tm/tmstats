2eaaf37c5f15d17ea92fe7d116cdc51ccbb2a316
David Singer <david@dsinger.com>
2018-10-25 21:32:16 -0700

    Bring up to current environment; PY3, etc.

--- doopenhouse.sh
+++ doopenhouse.sh
@@ -1,26 +1,15 @@
 #!/bin/sh
-# DreamHost setup for statistics.
-export TZ=PST8PDT
-export PYTHONPATH="$HOME/python:$PYTHONPATH"
+. setup.sh
 
-# Ensure we're in the right virtual environment
-export VIRTUAL_ENV_DISABLE_PROMPT=1
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/lib
-[[ -e ~/python2.7.11/bin ]] && . ~/python2.7.11/bin/activate
+cd "$data"
 
-
-# Even if we're running in a weird shell, let's use THIS directory as the current directory
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-data="$SCRIPTPATH/data"
-cd "$data"   # Run in the data directory.
-
-export STATS_HOME="$SCRIPTPATH"
-export STATS_DATA="$STATS_HOME/data"
 echo "<p>Getting new roster</p>"
 ../getroster.sh
 echo "<p>Processing Open House Report</p>"
+echo "<p>Using roster file '$(cat rosterondropbox.txt)' from Dropbox</p>"
+
 echo "<pre>"
-../openhouse.py && cp openhouseclubs.html ~/files/reports/ && ../clearcache.py district-programs
+../openhouse.py && isreal && cp openhouseclubs.html ~/files/reports/ && ../clearcache.py district-programs
 echo "</pre>"
 echo "<p>Open House Report has been updated on the <a href="/district-programs#openhousechallenge">District Programs</a> page.</p>"
 
