17ab4fec290e377aad3b4b64fde105e07470af6e
David Singer <david@dsinger.com>
2018-10-25 21:31:12 -0700

    Don't accept XLS/XLSX files

--- getroster.sh
+++ getroster.sh
@@ -3,4 +3,4 @@
 # Ingest roster and process awards
 
 cd $STATS_DATA
-$STATS_HOME/getfromdropbox.py --outfile latestroster.+ --namefile rosterfilename.txt --cfile rostercursor.txt --dbfilenamefile rosterondropbox.txt --ext csv xls xlsx --dir roster  && $STATS_HOME/ingestroster.py $(cat rosterfilename.txt)
+$STATS_HOME/getfromdropbox.py --outfile latestroster.+ --namefile rosterfilename.txt --cfile rostercursor.txt --dbfilenamefile rosterondropbox.txt --ext csv  --dir roster  && $STATS_HOME/ingestroster.py $(cat rosterfilename.txt)
