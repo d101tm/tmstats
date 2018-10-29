954bb5574217c4af1a14cb1315a548c65e0159da
David Singer <david@dsinger.com>
2018-10-29 15:47:17 -0700

    Issue message based on what happened

--- doopenhouse.sh
+++ doopenhouse.sh
@@ -9,7 +9,16 @@ echo "<p>Processing Open House Report</p>"
 echo "<p>Using roster file '$(cat rosterondropbox.txt)' from Dropbox</p>"
 
 echo "<pre>"
-../openhouse.py && isreal && cp openhouseclubs.html ~/files/reports/ && ../clearcache.py district-programs
+../openhouse.py && isreal && cp openhouseclubs.html ~/files/reports/ 
+src=$?
+../clearcache.py district-programs
 echo "</pre>"
-echo "<p>Open House Report has been updated on the <a href="/district-programs#openhousechallenge">District Programs</a> page.</p>"
+
+if (( $src  == 0 )) ; then 
+    echo "<p>Open House Report has been updated on the <a href="/district-programs#openhousechallenge">District Programs</a> page.</p>"
+elif isreal ; then
+    echo "<p>Report not copied, rc=$src"
+else
+    echo "<p>Not on d101tm.org, so not copied."
+fi
 
