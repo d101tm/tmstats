8580467b1ddb817480819a18a21c2b8784f8e21b
David Singer <webmaster@d101tm.org>
2019-09-18 17:50:52 -0700

    Create recognition list, too

--- updateanniversaryopenhouses.py
+++ updateanniversaryopenhouses.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python3
-""" Update Google spreadsheet for anniversary open houses """
+""" Update Google spreadsheet for anniversary open houses and build recognition inclusions """
 
 
 from datetime import datetime, timedelta
-
 import gspread
-
 import tmglobals
-from tmutil import cleandate, normalize, getGoogleCredentials
+from tmutil import cleandate, normalize, getGoogleCredentials, getClubBlock
 
-globals = tmglobals.tmglobals()
+myglobals = tmglobals.tmglobals()
 
 
+class Clubinfo:
+    def __init__(self, labels, row):
+        self.labels = labels
+        for (k, v) in zip(labels, row):
+            setattr(self, k, v)
 
-### Insert classes and functions here.  The main program begins in the "if" statement below.
+    def __repr__(self):
+        return ', '.join([f'{l, getattr(self, l)}' for l in labels])
+
+class Winner:
+    def __init__(self, clubname):
+        self.clubname = clubname
+
 
 if __name__ == "__main__":
  
@@ -21,13 +30,17 @@ if __name__ == "__main__":
     
     # Establish parameters
     parms = tmparms.tmparms()
-    
-    # Add other parameters here
+    parms.add_argument('--outfile', default='amazing.txt')
+    parms.add_argument('--prize1', default='an Amazing Gift Basket ($45 value)')
+    parms.add_argument('--prize2', default='an even more Amazing Gift Basket ($70 value)')
+    parms.add_argument('--needed', default=3)
 
     # Do global setup
-    globals.setup(parms)
-    curs = globals.curs
-    conn = globals.conn
+    myglobals.setup(parms)
+    curs = myglobals.curs
+    conn = myglobals.conn
+
+    winners = [[],[]]  # List of clubnames for each level.
 
     # Get the spreadsheet
     gc = gspread.authorize(getGoogleCredentials())
@@ -37,20 +50,12 @@ if __name__ == "__main__":
     deltacolumn = labels.index('newmembersadded') + 1
     eligiblethroughcol = labels.index('eligiblethrough') + 1
 
-    qbasestr = "SELECT newmembers + addnewmembers as newcount FROM clubperf WHERE asof = %s and clubnumber = %s"
 
     # Get the final date in the database - we don't ask for data later than we have!
     curs.execute("SELECT MAX(asof) FROM clubperf")
     maxasof = curs.fetchone()[0]
 
-    class Clubinfo:
-        def __init__(self, labels, row):
-            self.labels = labels
-            for (k, v) in zip(labels, row):
-                setattr(self, k, v)
 
-        def __repr__(self):
-                return ', '.join([f'{l, getattr(self, l)}' for l in labels])
 
     for rownum, row in enumerate(values[1:], start=2):
         club = Clubinfo(labels, row)
@@ -79,14 +84,27 @@ if __name__ == "__main__":
             except:
                 pass
 
+        qbasestr = "SELECT newmembers + addnewmembers as newcount, clubname FROM clubperf WHERE asof = %s and clubnumber = %s"
+
         if maxasof >= startdate:
             curs.execute(qbasestr, (startdate, clubnumber))
             newstart = curs.fetchone()[0]
             curs.execute(qbasestr, (min(enddate, maxasof), clubnumber))
-            newend = curs.fetchone()[0]
+            (newend, clubname) = curs.fetchone()
             delta = newend - newstart
             sheet.update_cell(rownum, deltacolumn, delta)
             sheet.update_cell(rownum, eligiblethroughcol, enddate.strftime("%-m/%-d/%y"))
+            # Add the club to the proper winners list
+            winners[1 if delta >= parms.needed else 0].append(Winner(clubname))
 
-
+    # Now, write out the congratulatory text if there are any winners.
+    outfile = open(parms.outfile, 'w')
+    if len(winners[0]):
+        outfile.write(f'<p><b>Congratulations</b> to {getClubBlock(winners[0])}\nfor earning {parms.prize1}\n')
+        outfile.write('by holding an Open House during the month of their club anniversary!</p>\n')
+    if len(winners[1]):
+        outfile.write(f'<p><b>Congratulations</b> to {getClubBlock(winners[1])}\nfor earning {parms.prize2}\n')
+        outfile.write('by holding an Open House during the month of their club anniversary <b>and</b>\n')
+        outfile.write(f'adding at least {parms.needed} members within a month of their Open House!</p>\n')
+    outfile.close()
 
