11e12b6ea04ac084e74cb9bee0aee93fe61f7fb1
David Singer <webmaster@d101tm.org>
2019-09-27 14:24:37 -0700

    Set up environment correctly
    Don't assume the workdir is a subdirectory of the source directory.

--- dreamhost.sh
+++ dreamhost.sh
@@ -5,16 +5,15 @@ export I_AM_D101TM=1
 #export PYTHONPATH="$HOME/python:$PYTHONPATH"
 
 # Ensure we're in the right virtual environment
-export VIRTUAL_ENV_DISABLE_PROMPT=1
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/lib
-. ~/python2.7.11/bin/activate
+#export VIRTUAL_ENV_DISABLE_PROMPT=1
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/lib
+#. ~/python2.7.11/bin/activate
 
 
 # Even if we're running in a weird shell, let's use THIS directory as the current directory
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd "$SCRIPTPATH"
 
-touch data/dreamhost       # We were here!
 ./daily.sh "$*"
 ./hourly.sh "$*"
 
--- hourly.sh
+++ hourly.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 . setup.sh
 
 cd "$workdir"   # Run in the work directory.
@@ -10,11 +9,11 @@ cd "$SCRIPTPATH"
 ./dotraining101.sh
 
 # Update contest and training pages
-(cd "$workdir";../makecontestpage.py && isreal && cp contestschedule.html ~/files/reports/)
-(cd "$workdir";../maketrainingpage.py && isreal && cp trainingschedule.html ~/files/reports/)
+(cd "$workdir";$SCRIPTPATH/makecontestpage.py && isreal && cp contestschedule.html ~/files/reports/)
+(cd "$workdir";$SCRIPTPATH/maketrainingpage.py && isreal && cp trainingschedule.html ~/files/reports/)
 
 # Don't update open house results
-#(cd "$workdir";../openhouse.py && isreal && cp openhouseclubs.html ~/files/reports/)
+#(cd "$workdir";$SCRIPTPATH/openhouse.py && isreal && cp openhouseclubs.html ~/files/reports/)
 
 # Copy info from Dropbox
-(cd "$workdir"; isreal && ../copywebfiles.py)
+(cd "$workdir"; isreal && $SCRIPTPATH/copywebfiles.py)
