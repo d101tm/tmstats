e7aed11da39b9db6ae2e4e25471360eec62e184d
David Singer <webmaster@d101tm.org>
2020-07-26 21:10:42 -0700

    Another program, another script

---
+++ prospervirtually.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+. setup.sh
+
+cd "$workdir"
+
+echo "<p>Getting new roster</p>"
+"$SCRIPTPATH"/getroster.sh
+echo "<p>Processing Prosper Virtually Report</p>"
+echo "<p>Using roster file '$(cat rosterondropbox.txt)' from Dropbox</p>"
+
+echo "<pre>"
+"$SCRIPTPATH"/openhouse.py --renewto 3/31/2021 --sheetname "Prosper Virtually" --outfile "prospervirtuallyclubs.html" && isreal && cp prospervirtuallyclubs.html ~/files/reports/ 
+src=$?
+"$SCRIPTPATH"/clearcache.py district-programs
+echo "</pre>"
+
+if (( $src  == 0 )) ; then 
+    echo "<p>Prosper Virtually Report has been updated on the <a href="/district-programs#prospervirtuallychallenge">District Programs</a> page.</p>"
+elif isreal ; then
+    echo "<p>Report not copied, rc=$src"
+else
+    echo "<p>Not on d101tm.org, so not copied."
+fi
+
