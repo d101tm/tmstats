c9fb208bb36f51a88f427180a94ec289ccb43fa6
David Singer <david@dsinger.com>
2021-01-01 14:07:21 -0800

    Remove commented-out Python2 setup

--- dreamhost.sh
+++ dreamhost.sh
@@ -2,13 +2,6 @@
 # DreamHost setup for statistics.
 export TZ=PST8PDT
 export I_AM_D101TM=1
-#export PYTHONPATH="$HOME/python:$PYTHONPATH"
-
-# Ensure we're in the right virtual environment
-#export VIRTUAL_ENV_DISABLE_PROMPT=1
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/lib
-#. ~/python2.7.11/bin/activate
-
 
 # Even if we're running in a weird shell, let's use THIS directory as the current directory
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
