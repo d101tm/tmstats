b6dbd4196ff193cd068f03573f3ac7456e780dcd
David Singer <david@dsinger.com>
2021-02-19 18:06:13 -0800

    Get club information from the CLUBPERF table

--- makeinitialworkingalignment.py
+++ makeinitialworkingalignment.py
@@ -2,55 +2,60 @@
 """ Copy current club information to working alignment CSV file 
 
 Creates a CSV with the proper headers and information based on current
-clubs.  Copy that CSV into the Google Spreadsheet used by the alignment
+clubs.
+
+You must copy that CSV into the Google Spreadsheet used by the alignment
 committee to hold the working alignment.
 """
 
-import tmutil, sys
 import tmglobals
+import tmparms
+from simpleclub import Club
+from overridepositions import overrideClubPositions
+
+# Establish parameters
+parms = tmparms.tmparms()
+# Add other parameters here
+
+# Do global setup
 myglobals = tmglobals.tmglobals()
+myglobals.setup(parms)
+curs = myglobals.curs
+conn = myglobals.conn
+
+headers = [
+        "clubnumber",
+        "clubname",
+        "oldarea",
+        "newarea",
+        "likelytoclose",
+        "color",
+        "goalsmet",
+        "activemembers",
+        "meetingday",
+        "meetingtime",
+        "place",
+        "address",
+        "city",
+        "state",
+        "zip",
+        "country",
+        "latitude",
+        "longitude"
+        ]
+
+print(','.join(headers))
+# Get current club information from the clubperf file - that will include hidden and suspended clubs.
+curs.execute('SELECT clubnumber, clubname, division, area FROM clubperf WHERE entrytype = "L"')
+clubs = []
+for club in curs.fetchall():
+    clubs.append((club[0], club[1], club[2]+club[3]))
+
+# Sort by area
+clubs.sort(key=lambda club:club[2])
+
+# And print
+for club in clubs:
+    print(f'{club[0]},"{club[1]}",{club[2]}')
 
 
-
-### Insert classes and functions here.  The main program begins in the "if" statement below.
-
-if __name__ == "__main__":
- 
-    import tmparms
-    from simpleclub import Club
-    
-    # Establish parameters
-    parms = tmparms.tmparms()
-    # Add other parameters here
-
-    # Do global setup
-    myglobals.setup(parms)
-    curs = myglobals.curs
-    conn = myglobals.conn
-    
-    headers = [
-            "clubnumber",
-            "clubname",
-            "oldarea",
-            "newarea",
-            "likelytoclose",
-            "color",
-            "goalsmet",
-            "activemembers",
-            "meetingday",
-            "meetingtime",
-            "place",
-            "address",
-            "city",
-            "state",
-            "zip",
-            "country",
-            "latitude",
-            "longitude"
-            ]
-
-    print(','.join(headers))
-    clubs = Club.getClubsOn(curs)
-    for c in sorted(list(clubs.values()),key=lambda c:c.division+c.area):
-        print(('%s,"%s",%s%s' % (c.clubnumber, c.clubname, c.division, c.area)))
-    
