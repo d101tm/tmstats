68ba8716981c21828d14cb223080d5e01fa7a520
Gur Hoshen <ghoshen1@gmail.com>
2021-09-01 15:06:42 -0700

    Fix luckyclub output since Training chg to class var

--- makecap.py
+++ makecap.py
@@ -115,11 +115,11 @@ if __name__ == "__main__":
         
     ambassadors = []        
     for row in sheet.get_all_records(head=2, empty2zero=True):
-        if (row['First Name'] + row['Last Name']) == 0 or (row['First Name']==' '  or row['Last Name']==' '):
-            break
-        #print(row['First Name'],type(row['First Name']),row['Last Name'],type(row['Last Name']))
-        if type(row['First Name']) !=int or  type(row['Last Name'] !=int ):
-            ambassadors.append(Ambassador(row)) 
+        #if (row['First Name'] + row['Last Name']) == 0 or (row['First Name']==' '  or row['Last Name']==' '):
+        #    break
+        print(row['First Name'],type(row['First Name']),row['Last Name'],type(row['Last Name']))
+        #if type(row['First Name']) !=int or  type(row['Last Name'] !=int ):
+        ambassadors.append(Ambassador(row)) 
         
     # Sort by points, then name:
     ambassadors.sort(key=lambda r:r.sortkey())
